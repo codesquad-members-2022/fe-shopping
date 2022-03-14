b89898f9031dd139887b10314ca7a930fec8649f
BB-choi <choibobae0809@gmail.com>
2022-03-15 08:40:04 +0900

    feat: 헤더 카테고리 버튼 구현

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const app = express();
 const path = require("path");
 const port = 3000;
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/index.html"));
+  res.sendFile(path.join(__dirname + "/public" + "/index.html"));
 });
 
 app.listen(port, () => {
