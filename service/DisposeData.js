65210c0458d042fc7ec95855504774880c9d67c1
godhyeongman <88314186+godhyeongman@users.noreply.github.com>
2022-03-15 11:56:18 +0900

    add: 1차 검색 관련 데이터 탐색 로직 구현
    모든 데이터 탐색하며 검색어와 매치되는 데이터 탐색이 비효율적이라 판단.
    데이터 보관을 앞글자를 기준으로 결정
    (띄어쓰기 있을시 띄어쓰기후 첫글자도 첫글자로 처리)
    
    단어 입력시 입력단어의 첫글자를 토대로 데이터 필터링후
    그안에서 검색어와 동일한 단어 포함시만 데이터 반환
    
    문제점: 아이폰 검색시 아이폰13 같은 글자가 안나옴 이부분 해결 필요

---
+++ service/DisposeData.js
@@ -0,0 +1,45 @@
+const searchingData = require("../data/searchData.json");
+
+// 단어를 통해서 데이터와 비교
+function checkKeyWord(keyWord) {
+  const trimedKeyWord = keyWord.trim();
+  const checkSpace = trimedKeyWord.split(" ");
+  const firstWordarray = checkSpace.map((word) => getFirstWord(word));
+  const filterdData = filteringData(firstWordarray);
+  return checkMatchWord(trimedKeyWord, filterdData);
+}
+
+// 첫번째 글자로 데이터보관중 -> 글자로 데이터 확인후 리턴
+function getDataByFirstWord(firstWord) {
+  return searchingData[firstWord];
+}
+
+// 단어의 첫글자 리턴
+function getFirstWord(keyWord) {
+  return keyWord.split("")[0];
+}
+
+// 입력에 띄어쓰기가 있을경우를 위해
+function filteringData(firstWordarray) {
+  return firstWordarray.reduce((filterd, firstWord) => {
+    if (searchingData.hasOwnProperty(firstWord)) {
+      filterd.push(getDataByFirstWord(firstWord));
+    }
+    return filterd.flat();
+  }, []);
+}
+
+//검색어와 데이터가 동일한지 비교후 일치하는 단어들만 리턴
+function checkMatchWord(keyWord, data) {
+  const splitSpace = data.map((el) => el.split(" "));
+  return splitSpace.reduce((check, splitedData) => {
+    if (splitedData.includes(keyWord)) {
+      check.push(splitedData.join(" "));
+    }
+    return check;
+  }, []);
+}
+
+console.log(checkKeyWord(" 아식스"));
+
+// module.exports = { data };
