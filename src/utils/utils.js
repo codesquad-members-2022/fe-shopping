69498f75373e332b548d51a919e6de1fa0150715
Cola <bcad1591@gmail.com>
2022-03-15 16:59:13 +0900

    Etc: utils.js 추가

---
+++ src/utils/utils.js
@@ -0,0 +1,25 @@
+export const selector = (selector, base = document) => {
+  return base.querySelector(selector);
+};
+
+export const selectorAll = (selector, base = document) => {
+  return base.querySelectorAll(selector);
+};
+
+export const addClass = (className, element) => {
+  element.classList.add(className);
+};
+
+export const removeClass = (className, element) => {
+  element.classList.remove(className);
+};
+
+export const toggleClass = (className, element) => {
+  element.classList.toggle(className);
+};
+
+export const createElement = (tagName, className) => {
+  const element = document.createElement(tagName);
+  if (className) element.className = className;
+  return element;
+};
