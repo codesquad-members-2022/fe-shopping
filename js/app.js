0e2136dcfdd0a058afc538511bc773b561744646
Ubeom Kim <kimub1204@gmail.com>
2022-03-15 11:45:48 +0900

    style: _colors.scss 정리

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 import { bar } from './template/bar.js';
 import { header } from './template/header.js';
+import { carousel } from './template/carousel.js';
 
 const wrap = document.querySelector('.wrap');
 wrap.insertAdjacentHTML('afterbegin', bar);
 
 const container = document.querySelector('.container');
 container.insertAdjacentHTML('afterbegin', header);
+container.insertAdjacentHTML('afterbegin', carousel);
