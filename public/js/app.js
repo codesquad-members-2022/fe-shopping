ffddd1639b08f6b05249c2da972f20ee38e86f64
Ubeom Kim <kimub1204@gmail.com>
2022-03-15 16:53:22 +0900

    캐러셀 마크업, 스타일 완료

--- public/js/app.js
+++ public/js/app.js
@@ -7,4 +7,4 @@ wrap.insertAdjacentHTML('afterbegin', bar);
 
 const container = document.querySelector('.container');
 container.insertAdjacentHTML('afterbegin', header);
-// container.insertAdjacentHTML('afterend', carousel);
+container.insertAdjacentHTML('afterend', carousel);
