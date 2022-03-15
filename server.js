1859355e85fd31cb373b0ce7893d11cd21b17b8e
Ubeom Kim <kimub1204@gmail.com>
2022-03-15 14:50:26 +0900

    chore: express 서버에 데이터파일 res 설정 완료

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
+
+app.get('/header/data', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/json/header.json'));
+});
+
+app.get('/carousel/data', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/json/carousel.json'));
+});
 
 const port = 3000;
 app.listen(port, function () {
