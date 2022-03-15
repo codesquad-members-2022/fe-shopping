60b750c9700aaf50d9be211af9463b4554a99992
Muffin <jinlog9@gmail.com>
2022-03-16 01:36:06 +0900

    feat: 카테고리 메뉴 영역 위 아래로 펼쳐지듯이 애니메이션 구현

--- client/src/utils/util.js
+++ client/src/utils/util.js
@@ -1,1 +1,27 @@
 export const $ = className => document.querySelector(className);
+export const handleHeightBottomAnimate = obj => {
+  (function animate() {
+    obj.start += obj.value;
+    const maxHeight = parseInt(getComputedStyle(obj.element).maxHeight);
+    obj.element.style.maxHeight = `${parseInt(maxHeight * obj.start)}px`;
+    if (obj.start < obj.height) {
+      obj.raf = requestAnimationFrame(animate);
+    } else {
+      cancelAnimationFrame(obj.raf);
+    }
+  })();
+};
+
+export const handleHeightTopAnimate = obj => {
+  let raf = null;
+  (function animate() {
+    obj.start += obj.value;
+    obj.element.style.maxHeight = `${parseInt(obj.start)}px`;
+    if (obj.start >= 0) {
+      raf = requestAnimationFrame(animate);
+    } else {
+      cancelAnimationFrame(raf);
+      obj.parentElement.removeChild(obj.element);
+    }
+  })();
+};
