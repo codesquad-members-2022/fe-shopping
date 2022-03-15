2c6258dbcf4cf16faa262b046cd806d6121920aa
Muffin <jinlog9@gmail.com>
2022-03-16 02:54:05 +0900

    feat: 헤더 검색창 관련 기능 따로 분리 후 작업진행중

---
+++ client/src/classes/search/SearchWord.js
@@ -0,0 +1,26 @@
+import { $ } from '../../utils/util.js';
+import { RecentSearch } from '../../components/search/RecentSearch.js';
+
+export class SearchWord {
+  static recentWords = [];
+  turn;
+  constructor() {
+    this.turn = true;
+  }
+
+  removeAll() {}
+
+  turnOff() {
+    this.turn = false;
+  }
+}
+
+SearchWord.prototype.render = () => {
+  // 최근검색어가 없으면 리턴.
+  // 최근검색어가 있으면 최근검색어 컴포넌트 보여주기
+  if (SearchWord.recentWords.length === 0) return;
+  const inputKeyWordBox = $('.main-header__bottom-search-input');
+  const recentSearchBox = $('.search-recent');
+  recentSearchBox?.remove();
+  inputKeyWordBox.insertAdjacentHTML('afterend', RecentSearch(SearchWord.recentWords));
+};
--- client/src/events/inputKeyWordEvent.js
+++ client/src/events/inputKeyWordEvent.js
@@ -1,22 +1,39 @@
 import { $ } from '../utils/util.js';
-import { SearchKeyWord } from '../components/search/SearchKeyWord.js';
-import { keywords } from '../constants/data.js';
+import { SearchWord } from '../classes/search/SearchWord.js';
 
 export const inputKeyWordEvent = () => {
+  const searchWord = new SearchWord();
   const inputKeyWordBox = $('.main-header__bottom-search-input');
+  const inputKeyWordBtn = $('.main-header__bottom-search--btn');
+  let keyWordTimer;
+  let saveValue = '';
 
   inputKeyWordBox.addEventListener('keyup', ({ target }) => {
-    const keyword = target.value;
-    // fetch api
-    // fetch 보내기전 임의로 클라이언트에서 더미데이터로 테스팅
-    const searchKeyWordBox = $('.main-header__bottom-search-keyword');
-    if (!searchKeyWordBox) {
-      inputKeyWordBox.insertAdjacentHTML('afterend', SearchKeyWord(keywords['Ahkeyword']));
+    if (keyWordTimer) {
+      clearTimeout(keyWordTimer);
+    }
+    keyWordTimer = setTimeout(function () {
+      // fetch api
+      // fetch 보내기전 임의로 클라이언트에서 더미데이터로 테스팅
+      const searchKeyWordBox = $('.main-header__bottom-search-keyword');
+      if (target.value) {
+        // inputKeyWordBox.insertAdjacentHTML('afterend', SearchKeyWord(keywords['Ahkeyword']));
+        saveValue = target.value;
+      }
+    }, 500);
+  });
+
+  inputKeyWordBox.addEventListener('focus', () => {
+    if (searchWord.turn) {
+      searchWord.render();
     }
   });
 
-  inputKeyWordBox.addEventListener('focus', ({ target }) => {
-    // 최근검색어가 있으면 최근검색어 컴포넌트 보여주기
-    // 최근검색어가 없으면 리턴.
+  inputKeyWordBtn.addEventListener('click', () => {
+    console.log('search Icon');
+    if (saveValue) {
+      SearchWord.recentWords.push(saveValue);
+      saveValue = '';
+    }
   });
 };
