0f5b72f3598140810100167f2c7f40c140d45feb
Ubeom Kim <kimub1204@gmail.com>
2022-03-16 11:21:16 +0900

    feat: 슬라이드 기능 구현

--- public/js/app.js
+++ public/js/app.js
@@ -2,9 +2,11 @@ import { bar } from './template/bar.js';
 import { header } from './template/header.js';
 import { carousel } from './template/carousel.js';
 
-const wrap = document.querySelector('.wrap');
-wrap.insertAdjacentHTML('afterbegin', bar);
+document.addEventListener('DOMContentLoaded', () => {
+  const wrap = document.querySelector('.wrap');
+  wrap.insertAdjacentHTML('afterbegin', bar);
 
-const container = document.querySelector('.container');
-container.insertAdjacentHTML('afterbegin', header);
-container.insertAdjacentHTML('afterend', carousel);
+  const container = document.querySelector('.container');
+  container.insertAdjacentHTML('afterbegin', header);
+  container.insertAdjacentHTML('afterend', carousel);
+});
--- public/js/template/carousel.js
+++ public/js/template/carousel.js
@@ -38,32 +38,32 @@ export const carousel = `
       </div>
       <div class="carousel-lnb">
         <ul class="carousel-lnb__list">
-          <li class="carousel-lnb__item caroseul-lnb__item--active">
+          <li class="carousel-lnb__item caroseul-lnb__item--active" data-num="1">
             <a href="#" class="carousel-lnb__link">
               <img src="#" alt="#" class="carousel-lnb__img" />
             </a>
           </li>
-          <li class="carousel-lnb__item">
+          <li class="carousel-lnb__item" data-num="2">
             <a href="#" class="carousel-lnb__link">
               <img src="#" alt="#" class="carousel-lnb__img" />
             </a>
           </li>
-          <li class="carousel-lnb__item">
+          <li class="carousel-lnb__item" data-num="3">
             <a href="#" class="carousel-lnb__link">
               <img src="#" alt="#" class="carousel-lnb__img" />
             </a>
           </li>
-          <li class="carousel-lnb__item">
+          <li class="carousel-lnb__item" data-num="4">
             <a href="#" class="carousel-lnb__link">
               <img src="#" alt="#" class="carousel-lnb__img" />
             </a>
           </li>
-          <li class="carousel-lnb__item">
+          <li class="carousel-lnb__item" data-num="5">
             <a href="#" class="carousel-lnb__link">
               <img src="#" alt="#" class="carousel-lnb__img" />
             </a>
           </li>
-          <li class="carousel-lnb__item">
+          <li class="carousel-lnb__item" data-num="6">
             <a href="#" class="carousel-lnb__link">
               <img src="#" alt="#" class="carousel-lnb__img" />
             </a>
