c18cf96a5367a0dd9b2ca684a6494f16e80b0d8b
seungchanHan <seungchanhan00@gmail.com>
2022-03-17 01:34:17 +0900

    [ADD] 최근검색창 연결

--- express/public/js/main.js
+++ express/public/js/main.js
@@ -1,6 +1,8 @@
 import Dropdown from './search/dropdown.js';
-
+import InputEvent from './search/input-form/input-event.js';
 (() => {
   const dropdown = new Dropdown();
-  dropdown.btnAddEvent();
+  dropdown.addBtnEvent();
+  const inputEvent = new InputEvent();
+  inputEvent.addInputEvent();
 })();
