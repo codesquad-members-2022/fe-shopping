b6daeaac1bf950030eaa9f557f249ce438fdc384
JinJeon <nanyong01@gmail.com>
2022-03-16 10:32:52 +0900

    🌱🌻 폴더 구조 정리 및 스타일 일부 적용
    전체적인 폴더의 구조를 재정비했다.
    header.pug : 헤더의 내용만을 담은 pug파일을 생성했다.
    base.pug : 여러 페이지의 기초가 되는 pug파일로서 생성했다.
    /images : 이미지 파일을 담아두는 폴더로서 생성했다.
    server.j : 이미지 파일 url을 통해 이미지에 접근하도록 했다.

--- src/server.js
+++ src/server.js
@@ -1,16 +1,15 @@
 import express from "express";
+import mainRouter from "./router/mainRouter.js";
 
 const app = express();
-const handleHome = (req, res) => {
-  return res.render("index");
-};
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 
 app.use(express.static(process.cwd() + "/src/client"));
 app.use("/assets", express.static("assets"));
+app.use("/images", express.static("src/images"));
 
-app.get("/", handleHome);
+app.use("/", mainRouter);
 
 export default app;
