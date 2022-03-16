ac66c7aef3398fec434839f9396fdc168b85eeee
jm <jm.dev.study@gmail.com>
2022-03-16 15:49:25 +0900

    [feat] controller.js: 이벤트와 관련된 클래스 구현 - 드롭다운을 렌더하고 관련된 이벤트를 실행한다.

---
+++ client/src/controller.js
@@ -0,0 +1,23 @@
+import {DropDown} from './view/dropdown.js'
+
+export class Controller {
+  constructor() {
+    this.dropDown = new DropDown();
+  }
+
+  init() {
+    this.dropDown.renderSelectCategory();
+    this.inputSelectCategoryEvent();
+  }
+
+  inputSelectCategoryEvent() {
+    const $select = document.querySelector('.select__category');
+    $select.addEventListener('click', (e) => {
+      this.dropDown.showCategory();
+
+      if (e.target.nodeName === 'A') {
+        this.dropDown.selectCategory(e);
+      }
+    });
+  }
+}
