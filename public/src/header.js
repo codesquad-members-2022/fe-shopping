905da1311ed0303ba6489530fb2b44db7748e833
dlwnstjrzz <95525638+dlwnstjrzz@users.noreply.github.com>
2022-03-16 12:40:39 +0900

    STYLE: 불필요한 코드 삭제

--- public/src/header.js
+++ public/src/header.js
@@ -3,18 +3,16 @@ const fakeDB = fetch("http://localhost:3000/fakeDB")
 
 const $input = document.querySelector('.coupang-search');
 const $historyList = document.querySelectorAll('.latest-search-keyword>ol>li')
-console.log($input)
-$historyList[0].innerHTML = '2'
-console.log($historyList[0])
+
 $input.addEventListener('keydown', (e) => {
   if (e.keyCode === 13) {   //엔터 누르면 아래 기록 다 사라지고 결과창 렌더
     $historyList.forEach((list, idx) => {
       list.innerHTML = "";
     });
   }
-  console.log($input.value)
+
   fakeDB.then((json) => {
-    console.log(json)
+
     const filteredItems = json.items
       .filter(v => v["name"].includes($input.value))
       .sort((a, b) => b.views - a.views);
--- public/src/test.js
+++
@@ -1,4 +0,0 @@
-fetch("http://localhost:3000/fakeDB")
-  .then((res) => res.json())
-  .then(json => console.log(json));
-  
