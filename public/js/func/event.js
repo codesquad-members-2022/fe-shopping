8927fd24707982c3e04edbe650e2337d8823f0dc
Ubeom Kim <kimub1204@gmail.com>
2022-03-16 20:43:58 +0900

    feat: 카테고리 드롭다운 구현

--- public/js/func/event.js
+++ public/js/func/event.js
@@ -1,12 +1,16 @@
 import { moveCarousel } from './carousel.js';
 import { toggleDropdown, chooseCategory } from './input-category.js';
+<<<<<<< HEAD
 import { RecentSearch } from './search-recent.js';
 import { AutoComplete } from './search-auto.js';
+=======
+>>>>>>> 49ea395 (feat: 카테고리 드롭다운 구현)
 
 document.addEventListener('DOMContentLoaded', () => {
   moveCarousel();
   toggleDropdown();
   chooseCategory();
+<<<<<<< HEAD
 
   const recentSearch = new RecentSearch();
   recentSearch.createRecentSearch();
@@ -16,4 +20,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const autoComplete = new AutoComplete();
   autoComplete.createAutoComplete();
   autoComplete.destroyAutoComplete();
+=======
+>>>>>>> 49ea395 (feat: 카테고리 드롭다운 구현)
 });
