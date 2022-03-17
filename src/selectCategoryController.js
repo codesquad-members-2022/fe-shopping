62787cb5769021bea6dd3c9ba6d36916b4bdbe16
bangdler <zbthz90@gmail.com>
2022-03-17 18:36:58 +0900

    feat: add click event to select category
    click 시 open list hidden toggle, select-btn image toggle

--- src/selectCategoryController.js
+++ src/selectCategoryController.js
@@ -1,33 +1,32 @@
 
-function addSelectEvent() {
+function initSelectCategoryEvent() {
     const $selectBtn = document.querySelector('#select-btn')
 
-    $selectBtn.addEventListener('click', (e) => clickSelectHandler(e))
-    $selectBtn.addEventListener('focusout', (e) => focusoutSelectHandler(e))
+    $selectBtn.addEventListener('click', (e) => clickSelectBtnHandler(e))
+    $selectBtn.addEventListener('focusout', (e) => focusoutSelectBtnHandler(e))
 }
 
-function focusoutSelectHandler(e) {
+function focusoutSelectBtnHandler(e) {
     const $selectBtn = e.target
-    console.log('focus',e.target)
     const $selectOpenList = document.querySelector('.header__select--open')
 
     addHidden($selectOpenList)
-    toggleBtnDown($selectBtn)
+    changeBtnDownImg($selectBtn)
 }
 
 function addHidden(target) {
     target.classList.add('hidden')
 }
 
-function toggleBtnDown(target) {
+function changeBtnDownImg(target) {
     target.classList.remove('btn-up')
     target.classList.add('btn-down')
 }
 
-function clickSelectHandler(e) {
+function clickSelectBtnHandler(e) {
     const $selectBtn = e.target
     const $selectOpenList = document.querySelector('.header__select--open')
-    console.log('click', e.target)
+
     toggleHidden($selectOpenList)
     toggleBtnImage($selectBtn)
 }
@@ -53,4 +52,4 @@ function toggleBtnImage(target) {
     }
 }
 
-export { addSelectEvent }
+export { initSelectCategoryEvent }
