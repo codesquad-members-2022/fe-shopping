055d91464722beb20936b85c4e93bd8b6254ea6b
jm <jm.dev.study@gmail.com>
2022-03-17 23:48:00 +0900

    [fix] 클래스 SelectCategory의 메서드 수정 - reduce를 templateDropDownItem 메서드 안으로 넣었다. - showCategory 메서드의 삼항연산자 줄바꿈을 해주었다. (prettier 옵션 수정)

--- client/src/view/selectCategory.js
+++ client/src/view/selectCategory.js
@@ -9,22 +9,20 @@ export class SelectCategory {
     const categoryData = ['전체', '여성패션', '남성패션', '남녀 공용 패션', '유아동패션'];
     const template = `
       <ul class="select__drop-down">
-        ${categoryData.reduce((acc, cur) => acc + this.templateDropDownItem(cur), '')}
+        ${this.templateDropDownItem(categoryData)}
       </ul>
     `;
     this.$select.insertAdjacentHTML('beforeend', template);
   }
 
   templateDropDownItem(data) {
-    return `<li><a href="#">${data}</a></li>`;
+    return data.reduce((acc, cur) => acc + `<li><a href="#">${cur}</a></li>`, '');
   }
 
   showCategory() {
     const lastChild = this.$select.lastElementChild;
     lastChild.classList.toggle('focus');
-    const arrowText = lastChild.classList.contains('focus')
-      ? 'arrow_drop_up'
-      : 'arrow_drop_down';
+    const arrowText = lastChild.classList.contains('focus') ? 'arrow_drop_up' : 'arrow_drop_down';
     this.$select.querySelector('.material-icons').innerText = arrowText;
   }
 
