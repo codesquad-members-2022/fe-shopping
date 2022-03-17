ec2aadab47de19fbb0c508a3b063d150b8dc2c71
Muffin <jinlog9@gmail.com>
2022-03-18 01:26:12 +0900

    refactor: 재사용되는 부분 함수로 분리

--- client/src/events/rootEvent.js
+++ client/src/events/rootEvent.js
@@ -1,18 +1,15 @@
-import { $ } from '../utils/util.js';
+import { $, checkElementClass, toggleClass } from '../utils/util.js';
 
 export const rootEvent = () => {
-  const body = $('body');
-  body.addEventListener('click', ({ target }) => {
+  document.body.addEventListener('click', ({ target }) => {
     const selectListBox = $('.search-list');
     const selectSearchCategoryBox = $('.search-selectBox');
 
-    if (
-      (selectListBox && target.classList.contains('search--allBtn')) ||
-      (selectListBox && target.classList.contains('search-selectBox'))
-    ) {
+    if (selectListBox && checkElementClass({ target, checkClasses: ['search-selectBox', 'search--allBtn'] })) {
       return;
     } else {
       selectListBox?.remove();
+      // toggleClass(selectSearchCategoryBox, 'search--off search--on');
       selectSearchCategoryBox.classList.remove('search--on');
       selectSearchCategoryBox.classList.add('search--off');
     }
