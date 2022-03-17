51256d89cc2e61b727d397ccac86277a1a96eaa7
Muffin <jinlog9@gmail.com>
2022-03-18 01:25:19 +0900

    rename: 클래스 네이밍수정

--- client/src/components/search/SearchCategory.js
+++ client/src/components/search/SearchCategory.js
@@ -2,7 +2,7 @@ export const SearchCategory = data => {
   return /* html */ `
         <ul class="search-list">
           ${data.reduce((prev, cur, index) => {
-            return (prev += `<li class="search-searchItem" data-id=${index}>${cur}</li>`);
+            return (prev += `<li class="search-item" data-id=${index}>${cur}</li>`);
           }, '')}
         </ul>
     `;
