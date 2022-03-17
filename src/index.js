7ac5c59f64d30380ef74170ebf895515084b9c39
DESKTOP-88VMMCI\user <luzverde0314@gmail.com>
2022-03-18 00:07:20 +0900

    Feat: 자동완성 기능 구현(분리해야 함)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { addClickEventToElement } from "./utils/utils.js";
+import { addClickEventToElement, delay } from "./utils/utils.js";
 
 //카테고리 클릭 이벤트
 const $selectCategoryWrap = document.querySelector(".select-category-wrap");
@@ -34,8 +34,6 @@ document.addEventListener("click", (e) => {
 });
 
 //seach 이벤트
-const fakeDB = fetch("./data/fakeDB.json").then((res) => res.json());
-
 const $searchInput = document.querySelector(".search-input");
 const $searchList = document.querySelector(".search-list");
 
@@ -51,12 +49,13 @@ $searchInput.addEventListener("keyup", (e) => {
 });
 
 $searchInput.addEventListener("click", () => {
+  $searchList.classList.toggle("is-opened");
   if (keywordStorage.length === 0) {
+    $searchList.classList.toggle("is-opened");
     localStorage.clear();
     return;
   }
   if (!$searchInput.value) {
-    $searchList.classList.toggle("is-opened");
     $searchList.innerHTML = "";
     $searchList.innerHTML += `<div class="search-list__auto">최근 검색어</div>`;
 
@@ -77,24 +76,44 @@ $searchBtn.addEventListener("click", storageClear);
 function storageClear() {
   keywordStorage = [];
   localStorage.clear();
+  $searchList.classList.remove("is-opened");
 }
 
-// $searchInput.addEventListener("keyup", (e) => {
-//   if (e.key === "Enter") {
-//     $searchList.innerHTML = "";
+//검색어 자동완성
+const fakeDB = fetch("./data/fakeDB.json").then((res) => res.json());
 
-//     fakeDB.then((json) => {
-//       const searchList = json.products
-//         .filter((v) => v["search-keyword"].includes($searchInput.value))
-//         .sort((a, b) => b.views - a.views);
+$searchInput.addEventListener("keyup", () => {
+  $searchList.classList.add("is-opened");
+  delay(500).then(() => {
+    if (!$searchInput.value) {
+      if (keywordStorage.length === 0) {
+        $searchList.classList.remove("is-opened");
+        return;
+      }
+      $searchList.innerHTML = "";
+      $searchList.innerHTML += `<div class="search-list__auto">최근 검색어</div>`;
 
-//       for (let i = 0; i < 10; i++) {
-//         if (!searchList[i]) break;
+      const length = keywordStorage.length - 1;
+      for (let i = 0; i < 8; i++) {
+        if (!keywordStorage[length - i]) break;
 
-//         const content = document.createElement("li");
-//         content.textContent = searchList[i]["search-keyword"];
-//         $searchList.appendChild(content);
-//       }
-//     });
-//   }
-// });
+        const keyword = document.createElement("li");
+        keyword.textContent = localStorage.getItem(keywordStorage[length - i]);
+        $searchList.appendChild(keyword);
+      }
+    } else {
+      $searchList.innerHTML = "";
+      fakeDB.then((json) => {
+        const searchList = json.products
+          .filter((v) => v["search-keyword"].includes($searchInput.value))
+          .sort((a, b) => b.views - a.views);
+        for (let i = 0; i < 10; i++) {
+          if (!searchList[i]) break;
+          const content = document.createElement("li");
+          content.textContent = searchList[i]["search-keyword"];
+          $searchList.appendChild(content);
+        }
+      });
+    }
+  });
+});
