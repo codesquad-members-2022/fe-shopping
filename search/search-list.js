526a99bbae275ae2bf071893e96a812600bf0480
nanjeong <whdwkxj7@naver.com>
2022-03-17 17:00:06 +0900

    refactor: search.js에서 SearchList 클래스의 메소드로 옮길 수 있는 것 옮김

--- search/search-list.js
+++ search/search-list.js
@@ -1,20 +1,38 @@
 class SearchList {
-    constructor() {
-        this.searchList = [];
-        this.isVisible = true;
-        // this.searchListBlock = getSearchListTemplate();
+    constructor(searchList, listContainer) {
+        this.searchListNode = searchList;
+        this.listContainer = listContainer;
+        this.searchItems = [];
+        this.isVisible = false;
     }
 
     show() {
         this.isVisible = true;
+        this.searchListNode.style.display = "block";
     }
 
     hide() {
         this.isVisible = false;
+        this.searchListNode.style.display = "none";
     }
 
     addSearchWord(word) {
-        this.searchList.unshift(word);
+        this.searchItems.unshift(word);
+        if (this.searchItems.length > 10) {
+            this.searchItems = this.searchItems.slice(0, 10);
+        }
+    }
+
+    getSearchListItem = (item) => {
+        return `<li class="search__list--item">${item}</li>`;
+    };
+
+    renderSearchList() {
+        const searchList = this.searchItems.reduce(
+            (acc, item) => acc + this.getSearchListItem(item),
+            ""
+        );
+        this.listContainer.innerHTML = searchList;
     }
 }
 
--- search/search.js
+++ search/search.js
@@ -1,20 +1,23 @@
 import { SearchList } from "./search-list.js";
 
 const searchbar = document.querySelector(".search__input");
-const searchList = document.querySelector(".search__list");
-const searchListContainer = document.querySelector(".search__list--container");
-const recentSearchList = new SearchList();
+const searchRecentList = document.querySelector(".search__recent-list");
+const searchRecentListContainer = document.querySelector(
+    ".search__recent-list--container"
+);
+const searchRelatedList = document.querySelector(".search__related-list");
+const searchRelatedListContainer = document.querySelector(
+    ".search__related-list--container"
+);
 
-const getSearchListItem = (item) => {
-    return `<li class="search__list--item">${item}</li>`;
-};
-
-const renderSearchList = (items) => {
-    const searchList = items
-        .slice(0, 10)
-        .reduce((acc, item) => acc + getSearchListItem(item), "");
-    searchListContainer.innerHTML = searchList;
-};
+const recentSearchList = new SearchList(
+    searchRecentList,
+    searchRecentListContainer
+);
+const relatedSearchList = new SearchList(
+    searchRelatedList,
+    searchRelatedListContainer
+);
 
 const getSearchWord = () => {
     const word = searchbar.value;
@@ -26,11 +29,10 @@ const getSearchWord = () => {
 const updateRecentSearchList = () => {
     const word = getSearchWord();
     recentSearchList.addSearchWord(word);
-    renderSearchList(recentSearchList.searchList);
+    recentSearchList.renderSearchList();
 };
 
 searchbar.addEventListener("focus", () => {
-    searchList.style.display = "block";
     recentSearchList.show();
 });
 
@@ -43,7 +45,7 @@ searchbar.addEventListener("keydown", (event) => {
 
 document.body.addEventListener("click", ({ target }) => {
     if (!target.closest(".search")) {
-        searchList.style.display = "none";
+        searchRecentList.style.display = "none";
         recentSearchList.hide();
     }
 });
