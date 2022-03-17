0d18d12c1ba1adbccab1a02d62817e76063fbcfb
seungchanHan <seungchanhan00@gmail.com>
2022-03-17 17:03:58 +0900

    [Add] 슬라이드 이미지 json

--- express/app.js
+++ express/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const carouselData = require('./public/data/carousel/carousel.json');
+
 const port = 5050;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/', (req, res) => {
+  res.json(carouselData.json);
 });
 
 app.use(express.static(__dirname));
