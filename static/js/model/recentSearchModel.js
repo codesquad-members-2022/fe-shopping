001a9df6633c1a4e230828448ea93a9472a19bfc
soralee <sora2821@gmail.com>
2022-03-18 12:12:39 +0900

    Feat: 최근검색어 9개 갯수 제한 구현

--- static/js/model/recentSearchModel.js
+++ static/js/model/recentSearchModel.js
@@ -10,8 +10,14 @@ export class RecentSearchModel {
 
   addKeyword(keyword) {
     const keywordList = Object.values(this.storage);
+    const keywordListMaxLength = 9;
     if (keywordList.includes(keyword)) {
       return;
+    }
+    if (keywordList.length >= keywordListMaxLength) {
+      const oldKeyword = keywordList[0];
+      this.keywordList.splice(0, 1);
+      this.storage.removeItem(oldKeyword);
     }
     this.storage.setItem(Date.now(), keyword);
     this.keywordList.push(keyword);
