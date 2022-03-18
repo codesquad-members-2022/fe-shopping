ef473065e263c3c98cb42f951c84eba08da4b764
Serin-Kim <serin9864@gmail.com>
2022-03-18 11:54:51 +0900

    Feat: 검색창 카테고리 선택 기능 추가

---
+++ public/data/carouselData.js
@@ -0,0 +1,20 @@
+export const carouselData = [
+  {
+    img: 'https://static.coupangcdn.com/la/cmg_paperboy/image/1647305691211/C1_PC1.jpg',
+  },
+  {
+    img: 'https://static.coupangcdn.com/qa/cmg_paperboy/image/1647243347428/0316_PC_C1.jpg',
+  },
+  {
+    img: 'https://static.coupangcdn.com/ja/cmg_paperboy/imag…A7%80%ED%84%B8%EC%9C%84%ED%81%AC_SMD-16096_PC.jpg',
+  },
+  {
+    img: 'https://static.coupangcdn.com/la/cmg_paperboy/imag…A7%88%EC%8A%A4%ED%81%AC%ED%8C%A9_SMD-16103_PC.jpg',
+  },
+  {
+    img: 'https://image6.coupangcdn.com/image/ccm/banner/67989d55f78458ca4fa9e635a9abad8b.png',
+  },
+  {
+    img: 'https://image7.coupangcdn.com/image/ccm/banner/5ff447c98b8ea03403616cff95c8649f.jpg',
+  },
+];
