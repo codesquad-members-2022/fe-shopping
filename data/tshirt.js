e2f1d706fe3f7eb69e9319416afba1ccbd0a5c31
nanjeong <whdwkxj7@naver.com>
2022-03-18 14:41:29 +0900

    feat: 데이터 추가

--- data/tshirt.js
+++ data/tshirt.js
@@ -1,4 +1,17 @@
 const t = [
+    { keyword: "탐사수" },
+    { keyword: "탄산수" },
+    { keyword: "토마토" },
+    { keyword: "빅토리아 탄산수" },
+    { keyword: "템포" },
+    { keyword: "트레비 탄산수" },
+    { keyword: "티비" },
+    { keyword: "클렌징 티슈" },
+    { keyword: "티니핑" },
+    { keyword: "스테비아 토마토" },
+];
+
+const ti = [
     { keyword: "티젠 콤부차" },
     { keyword: "티슈" },
     { keyword: "티비" },
@@ -9,6 +22,19 @@ const t = [
     { keyword: "티포트" },
     { keyword: "남자 티셔츠 긴팔티" },
     { keyword: "티젠 콤부차 레몬" },
+];
+
+const ts = [
+    { keyword: "사각 티슈" },
+    { keyword: "티셔츠" },
+    { keyword: "티슈" },
+    { keyword: "살균 티슈" },
+    { keyword: "손소독 티슈" },
+    { keyword: "소독 티슈" },
+    { keyword: "남성 티셔츠" },
+    { keyword: "주방 티슈" },
+    { keyword: "세탁소 티슈" },
+    { keyword: "빅사이즈 티셔츠" },
 ];
 
 const tshir = [
@@ -22,6 +48,19 @@ const tshir = [
     { keyword: "여성 티셔츠 긴팔티 기본티" },
     { keyword: "남아 티셔츠" },
     { keyword: "나이키 티셔츠" },
+];
+
+const tshirx = [
+    { keyword: "여성 티셔츠 긴팔티" },
+    { keyword: "여성 브이넥 티셔츠" },
+    { keyword: "남자 티셔츠 긴팔티" },
+    { keyword: "남성 티셔츠" },
+    { keyword: "빅사이즈 티셔츠" },
+    { keyword: "유아 티셔츠" },
+    { keyword: "강아지 티셔츠" },
+    { keyword: "주니어 티셔츠" },
+    { keyword: "아동 티셔츠" },
+    { keyword: "남성 빅사이즈 티셔츠" },
 ];
 
 const tshirt = [
@@ -38,8 +77,11 @@ const tshirt = [
 ];
 
 const database = new Map();
-database.set("티", t);
+database.set("ㅌ", t);
+database.set("티", ti);
+database.set("팃", ts);
 database.set("티셔", tshir);
+database.set("티셫", tshirx);
 database.set("티셔츠", tshirt);
 
 export { database };
