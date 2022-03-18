172382e1ee3cd43efffcc842e78064bc620b7971
Muffin <jinlog9@gmail.com>
2022-03-18 11:55:48 +0900

    feat: 최근검색, 자동완성 창 열려있을때 다른 곳 클릭했을때 닫히도록 클래스 따라 구현

--- client/src/events/rootEvent.js
+++ client/src/events/rootEvent.js
@@ -3,6 +3,8 @@ import { $, checkElementClass } from '../utils/util.js';
 export const rootEvent = () => {
   document.body.addEventListener('click', ({ target }) => {
     const selectListBox = $('.search-list');
+    const searchKeywordBox = $('.search-keyword');
+    const searchRecentBox = $('.search-recent');
 
     if (selectListBox && checkElementClass({ target, checkClasses: ['search-selectBox', 'search--allBtn'] })) {
       return;
@@ -12,6 +14,20 @@ export const rootEvent = () => {
       selectSearchCategoryBox.classList.remove('search--on');
       selectSearchCategoryBox.classList.add('search--off');
     }
+
+    if (
+      searchKeywordBox &&
+      checkElementClass({ target, checkClasses: ['search-keyword', 'main-header__input', 'main-header__input--btn'] })
+    ) {
+      return;
+    } else searchKeywordBox?.remove();
+
+    if (
+      searchRecentBox &&
+      checkElementClass({ target, checkClasses: ['search-recent', 'main-header__input', 'main-header__input--btn'] })
+    ) {
+      return;
+    } else searchRecentBox?.remove();
   });
 
   document.body.addEventListener('mouseover', ({ target }) => {
