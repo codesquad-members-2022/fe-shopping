167bc9ceb4899d3f50bf2d6bd9e631b4672550e0
Hemudi <ksum1205@naver.com>
2022-03-19 02:53:19 +0900

    Feat: 최근 검색 기록 저장을 위한 localStorage 관리 기능 구현

---
+++ public/src/util/localStorage.js
@@ -0,0 +1,19 @@
+const localStorageKey = 'HemPang';
+
+export const addLocalData = (key, arrayData) => {
+    const storedData = getLocalStorageData() || {
+        [key]: []
+    };
+    storedData[key].push(...arrayData);
+    localStorage.setItem(localStorageKey, JSON.stringify(storedData));
+}
+
+export const getLocalData = () => {
+    const storedData = localStorage.getItem(localStorageKey);
+    const parsedData = JSON.parse(storedData);
+    return parsedData;
+}
+
+export const clearLocalData = () => {
+    localStorage.clear();
+}
