2c4d131b0672dd7f0b4c5c962d559e715af7664a
seungchanHan <seungchanhan00@gmail.com>
2022-03-18 15:01:02 +0900

    [Mod] 스스륵 구현 중

--- express/public/js/search/dropdown.js
+++ express/public/js/search/dropdown.js
@@ -1,5 +1,6 @@
 import { $ } from '../utility/util.js';
 import { addEvent } from '../utility/util.js';
+import { categoryData } from '../../data/search-category/category.js';
 export default class Dropdown {
   addBtnEvent() {
     const $dropdownBtn = $('.search-category-dropdown');
@@ -20,17 +21,20 @@ export default class Dropdown {
 
   showOptionMenu($optionMenu) {
     $optionMenu.innerHTML = `
-    <ul class = "dropdown-menus">
-      <li>전체</li>
-      <li>뷰티</li>
-      <li>식품</li>
-      <li>여성패션</li>
-      <li>남성패션</li>
-      <li>주방용품</li>
-      <li>생활용품</li>
-    </ul>
+    <ul class="dropdown-menus">
+      ${categoryData.reduce(
+        (pre, curData) =>
+          pre +
+          `
+      <li>
+        <a href="#">${curData}</a>
+      </li>`,
+        ''
+      )}
+      </ul>
     `;
 
+    console.log($optionMenu);
     this.addClickEvent();
   }
 
