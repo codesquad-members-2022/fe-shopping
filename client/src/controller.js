3572b70dacab9d796a631769666a3539dba60fbc
jm <jm.dev.study@gmail.com>
2022-03-18 15:56:19 +0900

    [feat] controller.js: 최근검색어 이벤트와 자동검색어 이벤트 일부 구현 - focusInputHandle(): input에 포커스시 조건문에 따라 최근검색어 창이나 자동검색창을 보여준다. - typingInputHandle(): input에 타이핑하면 자동검색창을 보여준다. - submitFormHandle(): input에 값을 입력 후 엔터를 누르면 로컬스토리지에 값이 추가되고 검색창을 안보이게한다. - clickDropDownHandle(): 최근검색어창에서 리스트의 삭제 버튼과 전체삭제버튼 클릭시 발생하는 이벤트이다.

--- client/src/controller.js
+++ client/src/controller.js
@@ -1,22 +1,90 @@
 import { SelectCategory } from './view/selectCategory.js';
+import { SearchInput } from './view/searchInput.js';
+import { LocalStorage } from './model/localStorage.js';
 
 export class Controller {
   constructor() {
+    this.localStorage = new LocalStorage();
     this.selectCategory = new SelectCategory();
+    this.searchInput = new SearchInput(this.localStorage.keywordList);
+
+    this.selector = {
+      form: document.querySelector('.search__form'),
+      input: document.querySelector('.search__input'),
+      select: document.querySelector('.select__category'),
+      inputDropDown: document.querySelector('.input__drop-down'),
+    };
   }
 
   init() {
     this.setSelectCategoryEvents();
+    this.setSearchInputEvents();
+    this.setSearchFormEvents();
   }
 
   setSelectCategoryEvents() {
-    const $select = document.querySelector('.select__category');
-    $select.addEventListener('click', (e) => {
+    this.selector.select.addEventListener('click', (e) => {
       this.selectCategory.showCategory();
 
       if (e.target.nodeName === 'A') {
         this.selectCategory.selectCategory(e);
       }
     });
+  }
+
+  setSearchInputEvents() {
+    this.selector.input.addEventListener('focus', () => this.focusInputHandle());
+    this.selector.input.addEventListener('input', () => this.typingInputHandle());
+
+    this.selector.inputDropDown.addEventListener('mouseleave', () =>
+      this.searchInput.toggleInputFocusClass()
+    );
+  }
+
+  setSearchFormEvents() {
+    this.selector.form.addEventListener('submit', (e) => this.submitFormHandle(e));
+    this.selector.form.addEventListener('click', (e) => this.clickDropDownHandle(e));
+  }
+
+  focusInputHandle() {
+    if (!this.selector.input.value && !this.localStorage.keywordList.length) return;
+
+    if (this.selector.input.value) {
+      this.searchInput.updateAutoComplete();
+      this.searchInput.toggleInputFocusClass();
+    } else if (this.localStorage.keywordList.length > 0) {
+      this.selector.inputDropDown.classList.remove('auto-complete');
+      this.selector.inputDropDown.classList.add('recent-search');
+      this.searchInput.updateRecentSearchList(this.localStorage.keywordList);
+    }
+    this.searchInput.toggleInputFocusClass();
+  }
+
+  typingInputHandle() {
+    this.selector.input.value
+      ? this.searchInput.updateAutoComplete()
+      : this.searchInput.toggleInputFocusClass();
+  }
+
+  submitFormHandle(e) {
+    e.preventDefault();
+
+    if (!this.selector.input.value) return;
+    this.localStorage.addKeywordList(this.selector.input.value);
+    this.searchInput.toggleInputFocusClass();
+    this.searchInput.resetInputText();
+  }
+
+  clickDropDownHandle(e) {
+    if (e.target.classList.contains('delete__btn')) {
+      this.localStorage.removeKeywordList(e);
+      this.searchInput.updateRecentSearchList(this.localStorage.keywordList);
+    }
+
+    if (e.target.classList.contains('options--clear-keyword')) {
+      this.localStorage.clearKeywordList();
+      this.searchInput.resetRecentSearchList();
+      this.searchInput.toggleInputFocusClass();
+    }
   }
 }
