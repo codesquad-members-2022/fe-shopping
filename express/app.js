a46546e5b4e165baf1d1272508a2e96ef6d813a8
seungchanHan <seungchanhan00@gmail.com>
2022-03-18 12:35:23 +0900

    [Add] 자동완성 json 받아오기

--- express/app.js
+++ express/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const carouselData = require('./public/data/carousel/carousel.json');
+const gaData = require('./public/data/auto-complete/ga.json');
+const naData = require('./public/data/auto-complete/na.json');
 
 const port = 5050;
 
@@ -10,6 +12,13 @@ app.get('/', (req, res) => {
 
 app.get('/', (req, res) => {
   res.json(carouselData.json);
+});
+
+app.get('/', (req, res) => {
+  res.json(gaData.json);
+});
+app.get('/', (req, res) => {
+  res.json(naData.json);
 });
 
 app.use(express.static(__dirname));
