988cf8f131d6ed72c8d0d11aa93e949c29ca7a68
Serin-Kim <serin9864@gmail.com>
2022-03-21 18:04:07 +0900

    Feat: 검색창 auto complete 기능 추가

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
+const autocompleteData = require('./public/data/autocompleteData.json');
 const port = 8000;
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/autocomplete', (req, res) => {
+  res.send(JSON.stringify(autocompleteData[req.query.value]));
+  // res.json(autocompleteData);
 });
 
 app.listen(port, () => {
--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,5 @@
 import { carouselData } from '../data/carouselData.js';
+import { delay } from './util.js';
 
 /* 캐러셀 구현하려다가 첫 이미지만 넣고 검색 기능 구현 중 */
 const carouselSection = document.querySelector('.carousel-banner');
@@ -18,4 +19,33 @@ selectCategory.addEventListener('click', ({ target }) => {
   selectCategoryOption.classList.toggle('showCategory');
   if (!target.closest('.select-category-option')) return;
   selectCategoryContent.innerHTML = target.textContent;
+});
+
+/* 검색창 자동완성 */
+
+const getAutocomplete = (value) => {
+  return fetch(`/autocomplete?value=${value}`).then((res) => res.json());
+};
+
+const showAutocomplete = (data) => {
+  // data.forEach((e) => console.log(e));
+  let template = `<div> `;
+  data.forEach((element) => {
+    template += `<li>${element}</li>`;
+  });
+  template += `</div>`;
+
+  autocompleteBox.innerHTML = template;
+};
+
+const searchInput = document.querySelector('.search-form-input');
+const autocompleteBox = document.querySelector('.autocomplete-popup');
+
+searchInput.addEventListener('input', ({ target }) => {
+  autocompleteBox.classList.add('showAutocomplete');
+  delay(500).then((res) => {
+    getAutocomplete(target.value).then((data) => {
+      showAutocomplete(data);
+    });
+  });
 });
