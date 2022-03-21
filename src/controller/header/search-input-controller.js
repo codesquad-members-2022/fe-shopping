df8c84efd9d9eb81f7e733cbf9451e3cbc0d62b1
DESKTOP-88VMMCI\user <luzverde0314@gmail.com>
2022-03-22 01:40:43 +0900

    Refactor: SearchInputController 이벤트 핸들러 함수 클래스 메서드로 삽입

--- src/controller/header/search-input-controller.js
+++ src/controller/header/search-input-controller.js
@@ -1,4 +1,4 @@
-import { delay } from "../../utils/utils.js";
+import { debounce } from "../../utils/utils.js";
 
 export class SearchInputController {
   constructor() {
@@ -12,98 +12,77 @@ export class SearchInputController {
   }
   addEvents() {
     this.$searchInput.addEventListener("keyup", (e) =>
-      currentKeywordHandler(e, "Enter", this.keywordStorage, this.$searchInput)
+      this.inputEnterHandler(e)
     );
-    this.$searchInput.addEventListener(
-      "click",
-      inputClickHandler(
-        this.$searchInput,
-        this.$searchList,
-        "is-opened",
-        this.keywordStorage
-      )
-    );
-    this.$searchBtn.addEventListener("click", () =>
-      storageClear(this.keywordStorage, this.$searchList, "is-opened")
-    );
+    this.$searchInput.addEventListener("click", () => this.inputClickHandler());
+    this.$searchBtn.addEventListener("click", this.storageClear);
     this.$searchInput.addEventListener("keyup", () =>
-      autoCompleteHandler(
-        this.$searchInput,
-        this.$searchList,
-        "is-opened",
-        this.keywordStorage,
-        500,
-        this.getData()
-      )
+      this.autoCompleteHandler(500, this.getData())
     );
+  }
+
+  inputEnterHandler(e) {
+    if (e.key === "Enter") {
+      localStorage.setItem(
+        `${this.$searchInput.value}`,
+        `${this.$searchInput.value}`
+      );
+      this.keywordStorage.push(this.$searchInput.value);
+      this.$searchInput.value = "";
+    }
+  }
+
+  inputClickHandler() {
+    this.$searchList.classList.add("is-opened");
+    if (!this.$searchInput.value) {
+      if (this.keywordStorage.length === 0) {
+        this.$searchList.classList.remove("is-opened");
+        return;
+      }
+      this.$searchList.innerHTML = "";
+      this.$searchList.innerHTML += `<div class="search-list__auto">최근 검색어</div>`;
+
+      const length = this.keywordStorage.length - 1;
+      for (let i = 0; i < 8; i++) {
+        if (!this.keywordStorage[length - i]) break;
+
+        const keyword = document.createElement("li");
+        keyword.textContent = localStorage.getItem(
+          this.keywordStorage[length - i]
+        );
+        this.$searchList.appendChild(keyword);
+      }
+    }
+  }
+
+  storageClear() {
+    this.keywordStorage = [];
+    localStorage.clear();
+    this.$searchList.classList.remove("in-opened");
+  }
+
+  autoCompleteHandler(interval, data) {
+    this.$searchList.classList.add("is-opened");
+
+    debounce(interval).then(() => {
+      if (!this.$searchInput.value) {
+        this.inputClickHandler();
+      } else {
+        this.$searchList.innerHTML = "";
+        data.then((json) => {
+          const searchList = json.products
+            .filter((v) =>
+              v["search-keyword"].includes(this.$searchInput.value)
+            )
+            .sort((a, b) => b.views - a.views);
+          for (let i = 0; i < 10; i++) {
+            if (!searchList[i]) break;
+            const content = document.createElement("li");
+            content.textContent = searchList[i]["search-keyword"];
+            this.$searchList.appendChild(content);
+          }
+        });
+      }
+    });
   }
 }
-
-const currentKeywordHandler = (e, key, storageArr, targetElement) => {
-  if (e.key === key) {
-    localStorage.setItem(`${targetElement.value}`, `${targetElement.value}`);
-    storageArr.push(targetElement.value);
-    targetElement.value = "";
-  }
-};
-
-const inputClickHandler = (
-  inputElement,
-  ulElement,
-  toggleClass,
-  storageArr
-) => {
-  ulElement.classList.add(toggleClass);
-  if (!inputElement.value) {
-    if (storageArr.length === 0) {
-      ulElement.classList.remove(toggleClass);
-      return;
-    }
-    ulElement.innerHTML = "";
-    ulElement.innerHTML += `<div class="search-list__auto">최근 검색어</div>`;
-
-    const length = storageArr.length - 1;
-    for (let i = 0; i < 8; i++) {
-      if (!storageArr[length - i]) break;
-
-      const keyword = document.createElement("li");
-      keyword.textContent = localStorage.getItem(storageArr[length - i]);
-      ulElement.appendChild(keyword);
-    }
-  }
-};
-
-const storageClear = (storageArr, listElement, toggleClass) => {
-  storageArr = [];
-  localStorage.clear();
-  listElement.classList.remove(toggleClass);
-};
-
-const autoCompleteHandler = (
-  inputElement,
-  ulElement,
-  toggleClass,
-  storageArr,
-  interval,
-  data
-) => {
-  ulElement.classList.add(toggleClass);
-  delay(interval).then(() => {
-    if (!inputElement.value) {
-      inputClickHandler(inputElement, ulElement, toggleClass, storageArr);
-    } else {
-      ulElement.innerHTML = "";
-      data.then((json) => {
-        const searchList = json.products
-          .filter((v) => v["search-keyword"].includes(inputElement.value))
-          .sort((a, b) => b.views - a.views);
-        for (let i = 0; i < 10; i++) {
-          if (!searchList[i]) break;
-          const content = document.createElement("li");
-          content.textContent = searchList[i]["search-keyword"];
-          ulElement.appendChild(content);
-        }
-      });
-    }
-  });
-};
