b388332faeae2ac1376caa3866bbd38f19f4ea91
DESKTOP-88VMMCI\user <luzverde0314@gmail.com>
2022-03-22 16:54:05 +0900

    Feat: AutoCompleteStore 모델 생성

--- src/model/autocomplete-store.js
+++ src/model/autocomplete-store.js
@@ -1,5 +1,14 @@
-export class AutoComplete {
+export class AutoCompleteStore {
   constructor() {
-    this.data = "";
+    this.keywordData = [];
+  }
+  async getKeywordData(value) {
+    const data = await fetch("./data/fakeDB.json");
+    const json = await data.json();
+    const sorted = await json.products
+      .filter((v) => v.keyword.includes(value))
+      .sort((a, b) => b.views - a.views);
+
+    this.keywordData = sorted.map((v) => v.keyword);
   }
 }
