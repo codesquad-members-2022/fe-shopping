5a579530f587813df7e38cf32505dbe597d6ae15
DESKTOP-88VMMCI\user <luzverde0314@gmail.com>
2022-03-22 15:10:38 +0900

    Feat: toggleOn toggleOff 메서드 추가

--- src/controller/header/search-input-controller.js
+++ src/controller/header/search-input-controller.js
@@ -1,3 +1,4 @@
+import { CurrentSearchStore } from "../../model/current-search-store.js";
 import { debounce } from "../../utils/utils.js";
 
 export class SearchInputController {
@@ -5,7 +6,9 @@ export class SearchInputController {
     this.$searchInput = document.querySelector(".search-input");
     this.$searchList = document.querySelector(".search-list");
     this.$searchBtn = document.querySelector(".search-btn");
-    this.keywordStorage = [];
+    this.keywordStore = new CurrentSearchStore();
+    this.autoCompleteStore;
+    this.toggle = false;
   }
   getData() {
     return fetch("./data/fakeDB.json").then((res) => res.json());
@@ -15,54 +18,51 @@ export class SearchInputController {
       this.inputEnterHandler(e)
     );
     this.$searchInput.addEventListener("click", () => this.inputClickHandler());
-    this.$searchBtn.addEventListener("click", this.storageClear);
+    this.$searchBtn.addEventListener("click", () => this.storageClear());
     this.$searchInput.addEventListener("keyup", () =>
       this.autoCompleteHandler(500, this.getData())
     );
+    document.addEventListener("click", (e) => this.otherClickHandler(e));
   }
 
   inputEnterHandler(e) {
     if (e.key === "Enter") {
-      localStorage.setItem(
-        `${this.$searchInput.value}`,
-        `${this.$searchInput.value}`
-      );
-      this.keywordStorage.push(this.$searchInput.value);
+      localStorage.setItem(`${Date.now()}`, `${this.$searchInput.value}`);
+      this.keywordStore.localStorageArr.unshift(this.$searchInput.value);
       this.$searchInput.value = "";
     }
   }
 
   inputClickHandler() {
-    this.$searchList.classList.add("is-opened");
+    this.toggleOn();
+
     if (!this.$searchInput.value) {
-      if (this.keywordStorage.length === 0) {
-        this.$searchList.classList.remove("is-opened");
+      if (this.keywordStore.length === 0) {
+        this.toggleOff();
         return;
       }
+
       this.$searchList.innerHTML = "";
       this.$searchList.innerHTML += `<div class="search-list__auto">최근 검색어</div>`;
 
-      const length = this.keywordStorage.length - 1;
+      const length = this.keywordStore.localStorageArr.length;
       for (let i = 0; i < 8; i++) {
-        if (!this.keywordStorage[length - i]) break;
-
+        if (!length) break;
         const keyword = document.createElement("li");
-        keyword.textContent = localStorage.getItem(
-          this.keywordStorage[length - i]
-        );
+        keyword.textContent = this.keywordStore.localStorageArr[i];
         this.$searchList.appendChild(keyword);
       }
     }
   }
 
   storageClear() {
-    this.keywordStorage = [];
+    this.keywordStore = [];
     localStorage.clear();
-    this.$searchList.classList.remove("in-opened");
+    this.toggleOff();
   }
 
-  autoCompleteHandler(interval, data) {
-    this.$searchList.classList.add("is-opened");
+  async autoCompleteHandler(interval, data) {
+    this.toggleOn();
 
     debounce(interval).then(() => {
       if (!this.$searchInput.value) {
@@ -71,18 +71,34 @@ export class SearchInputController {
         this.$searchList.innerHTML = "";
         data.then((json) => {
           const searchList = json.products
-            .filter((v) =>
-              v["search-keyword"].includes(this.$searchInput.value)
-            )
+            .filter((v) => v.keyword.includes(this.$searchInput.value))
             .sort((a, b) => b.views - a.views);
           for (let i = 0; i < 10; i++) {
             if (!searchList[i]) break;
             const content = document.createElement("li");
-            content.textContent = searchList[i]["search-keyword"];
+            content.textContent = searchList[i].keyword;
             this.$searchList.appendChild(content);
           }
         });
       }
     });
+  }
+
+  otherClickHandler(e) {
+    if (e.target.className === "search-input") return;
+    if (e.target.parentNode.className !== "search-list is-opened") {
+      if (this.toggle) {
+        this.toggleOff();
+      }
+    }
+  }
+
+  toggleOn() {
+    this.$searchList.classList.add("is-opened");
+    this.toggle = true;
+  }
+  toggleOff() {
+    this.$searchList.classList.remove("is-opened");
+    this.toggle = false;
   }
 }
