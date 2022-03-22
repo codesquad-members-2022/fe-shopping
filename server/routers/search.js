5ad0cf7a4d3814937f1707470e187cd37b350d6d
jindonyy <rsefa94@gmail.com>
2022-03-23 00:11:41 +0900

    Refactor: searchRouter -> get 안의 익명함수로 수정

--- server/routers/search.js
+++ server/routers/search.js
@@ -2,14 +2,12 @@ const server = require('express');
 const router = server.Router();
 const searchKeywords = require('../data/searchKeywords.json');
 
-const searchRouter = (request, response) => {
+router.get('/', (request, response) => {
   try {
     response.json(searchKeywords);
   } catch (err) {
     response.status(500).send(err);
   }
-};
-
-router.get('/', searchRouter);
+});
 
 module.exports = router;
