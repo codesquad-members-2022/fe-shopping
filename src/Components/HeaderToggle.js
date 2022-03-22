1ce98f2e23d4c7459f17dbb2045677d758340910
godhyeongman <aaaa4000@naver.com>
2022-03-22 16:16:27 +0900

    refactor: HeaderToggle(Model) 구조 수정

--- src/Components/HeaderToggle.js
+++ src/Components/HeaderToggle.js
@@ -1,37 +1,47 @@
 import { Toggle } from "./AbstractToggle.js";
 
-function SearchInputToggle(liContents) {
-  this.liContents = liContents;
+function SearchInputToggle() {
   this.liClassName = "search--toggle--li";
   this.ulClassName = "search--toggle--ul";
-  this.dom = document.createElement("ul");
-  this.dom.classList.add(this.ulClassName);
-  this.dom.innerHTML = this.getHTML();
 }
 
-function SearchMenuToggle(liContents) {
-  this.liContents = liContents;
+function SearchMenuToggle() {
   this.liClassName = "search--menu--li";
   this.ulClassName = "search--menu--ul";
-  this.dom = document.createElement("ul");
-  this.dom.classList.add(this.ulClassName);
-  this.dom.innerHTML = this.getHTML();
 }
 
 SearchInputToggle.prototype = Object.create(Toggle.prototype);
 SearchMenuToggle.prototype = Object.create(Toggle.prototype);
 
-SearchInputToggle.prototype.getHTML = function () {
-  if (this.isEmptyArr(this.liContents)) {
+SearchInputToggle.prototype.getHTML = function (liContents) {
+  if (this.isEmptyArr(liContents)) {
     return;
   } // 컨텐츠 없으면 return undefined
 
-  return (
-    /*html*/ `${this.liContents.reduce((liHtml, contents) => {
+  const DOM = document.createElement("ul");
+  DOM.classList.add(this.ulClassName);
+  DOM.innerHTML =
+    /*html*/ `${liContents.reduce((liHtml, contents) => {
       liHtml += `<li class="${this.liClassName}">${contents}</li>`;
       return liHtml;
     }, "")}` +
-    '<button type="button" class="header__main--deleteHistoryBtn">기록 전체삭제</button>'
-  );
+    '<button type="button" class="header__main--deleteHistoryBtn">기록 전체삭제</button>';
+
+  return DOM;
+};
+
+SearchMenuToggle.prototype.getHTML = function (liContents) {
+  if (this.isEmptyArr(liContents)) {
+    return;
+  } // 컨텐츠 없으면 return undefined
+
+  const DOM = document.createElement("ul");
+  DOM.classList.add(this.ulClassName);
+  DOM.innerHTML = /*html*/ `${liContents.reduce((liHtml, contents) => {
+    liHtml += `<li class="${this.liClassName}">${contents}</li>`;
+    return liHtml;
+  }, "")}`;
+
+  return DOM;
 };
 export { SearchInputToggle, SearchMenuToggle };
