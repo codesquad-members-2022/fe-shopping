e5b67805a9c728e8c58527faea64a03ec86d6f29
seungchanHan <seungchanhan00@gmail.com>
2022-03-22 22:31:22 +0900

    [Add] 디바운스 쓰루틀링 구현

--- express/public/js/utility/util.js
+++ express/public/js/utility/util.js
@@ -1,4 +1,6 @@
-export const $ = (selected) => document.querySelector(selected);
+export const $ = (selected) => document.body.querySelector(selected);
+
+export const $$ = (selected) => document.body.querySelectorAll(selected);
 
 export async function fetchData(url) {
   const successData = await fetch(url);
@@ -16,16 +18,39 @@ export async function getCompleteData(consonant) {
 
 export function makeImageSlide(list) {
   return `
-  <li class="image-element">
+  <li id= "${list.id}" class="image-element">
     <img src="${list.imgURL}" / alt="${list.imgTitle} 이미지" width = "950" height = "400">
     </li>
   `;
 }
 
-export function makeSideTeb(list) {
+export function makeSideTab(list) {
   return `
-  <li class="side-teb-element">
+  <li id= "${list.id}" class="side-tab-element">
     <img src="${list.subImgURL}" / alt="${list.imgTitle} 이미지" width = "200" height = "55">
     </li>
   `;
+}
+
+export function throttle(callback, wait) {
+  let waiting = true;
+  return function (...args) {
+    if (waiting) {
+      callback.apply(this, args);
+      waiting = false;
+      setTimeout(() => {
+        waiting = true;
+      }, wait);
+    }
+  };
+}
+
+export function debounce(callback, wait) {
+  let waiting;
+  return function (...args) {
+    clearTimeout(waiting);
+    waiting = setTimeout(() => {
+      callback.apply(this, args);
+    }, wait);
+  };
 }
