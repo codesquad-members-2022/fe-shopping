4c20ec59e3f62b810a3433675ad76c18a1592dd7
nanjeong <whdwkxj7@naver.com>
2022-03-23 18:09:35 +0900

    build: 데모 페이지를 위한 설정 추가

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const dataOfTshirt = require("./data/tshirt.js");
 const path = require("path");
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use("/static", express.static(path.join(__dirname, "/public")));
 
