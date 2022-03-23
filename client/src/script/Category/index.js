bb9d8311304b34d009810772d4c13a0bddc7cbd5
ldldz <qkrqudrjs4350@gmail.com>
2022-03-23 16:04:15 +0900

    Feat: 카테고리 데이터 서버에서 응답

--- client/src/script/Category/index.js
+++ client/src/script/Category/index.js
@@ -1,20 +1,15 @@
+import { getCategories } from "../api.js";
 import Controller from "./controller/Controller.js";
 import Model from "./models/Model.js";
 import CategoryButton from "./views/CategoryButton.js";
 import LargeCategory from "./views/LargeCategory.js";
 
-const largeCategoryData = {
-  largeCategories: [
-    '패션의류',
-    '뷰티',
-    '출산/유아동',
-    '식품',
-    '주방용품',
-    '생활용품',
-    '등등등'
-  ]
-};
-const categoryButton = new Controller(new CategoryButton('.category-button'), null);
-const largeCategory = new Controller(new LargeCategory('.category-wrapper'), new Model(largeCategoryData));
-categoryButton.addEventListener('mouseenter', () => largeCategory.show());
-categoryButton.addEventListener('mouseleave', () => largeCategory.hide());
+async function init () {
+  const largeCategories = await getCategories('large');
+  const categoryButton = new Controller(new CategoryButton('.category-button'), null);
+  const largeCategory = new Controller(new LargeCategory('.category-wrapper'), new Model(largeCategories));
+  categoryButton.addEventListener('mouseenter', () => largeCategory.show());
+  categoryButton.addEventListener('mouseleave', () => largeCategory.hide());
+}
+
+init();
--- client/src/script/api.js
+++ client/src/script/api.js
@@ -3,5 +3,10 @@ const getAutoComplete = (keyword) => {
     .then((res) => res.json())
     .then((data) => data);
 };
+const getCategories = async (type) => {
+  const res = await fetch(`/category?type=${type}`);
+  const data = res.json();
+  return data;
+}
 
-export { getAutoComplete };
+export { getAutoComplete, getCategories };
