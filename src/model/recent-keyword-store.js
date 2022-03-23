b8bad60a47fafaccb8dae1241adedda618b19505
DESKTOP-88VMMCI\user <luzverde0314@gmail.com>
2022-03-23 11:39:16 +0900

    Rename: 최근검색어 클래스 이름 수정

---
+++ src/model/recent-keyword-store.js
@@ -0,0 +1,21 @@
+export class RecentKeywordStore {
+  constructor() {
+    this.getLocalStorage();
+    this.maxListNum = 8;
+  }
+
+  getLocalStorage() {
+    this.localStorageArr = [];
+    const storage = [];
+    for (let i = 0; i < localStorage.length; i++) {
+      storage.push(localStorage.key(i));
+    }
+    storage
+      .sort((a, b) => b - a)
+      .forEach((key) => this.localStorageArr.push(localStorage.getItem(key)));
+
+    if (this.localStorageArr.length > this.maxListNum) {
+      this.localStorageArr = this.localStorageArr.splice(0, this.maxListNum);
+    }
+  }
+}
