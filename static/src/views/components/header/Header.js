ad3c23ac45cf5aa468d833cebeadb52610590bc3
jindonyy <rsefa94@gmail.com>
2022-03-23 19:00:51 +0900

    Feat: prototpye 사용해보기

--- static/src/views/components/header/Header.js
+++ static/src/views/components/header/Header.js
@@ -4,23 +4,21 @@ import { SearchBar } from './search/SearchBar.js';
 import { RecentSearchKeywords } from './search/RecentSearchKeywords.js';
 import { AutomaticCompletion } from './search/AutomaticCompletion.js';
 
-export class Header {
-  constructor() {
-    this.RECENT_KEYWORDS_STORAGE_KEY = 'recentSearchKeywords';
-    this.searchStorage = localStorage;
-    this.searchCategories = new SearchCategories();
-    this.searchCategoriesButton = new SearchCategoriesButton();
-    this.recentSearchKeywords = new RecentSearchKeywords(this.RECENT_KEYWORDS_STORAGE_KEY, this.searchStorage);
-    this.automaticCompletion = new AutomaticCompletion();
-    this.searchBar = new SearchBar();
-    this.initSearchArea();
-  }
+export const Header = function () {
+  this.RECENT_KEYWORDS_STORAGE_KEY = 'recentSearchKeywords';
+  this.searchStorage = localStorage;
+  this.searchCategories = new SearchCategories();
+  this.searchCategoriesButton = new SearchCategoriesButton();
+  this.recentSearchKeywords = new RecentSearchKeywords(this.RECENT_KEYWORDS_STORAGE_KEY, this.searchStorage);
+  this.automaticCompletion = new AutomaticCompletion();
+  this.searchBar = new SearchBar();
+  this.initSearchArea();
+};
 
-  initSearchArea() {
-    this.searchCategories.init(this.searchCategoriesButton);
-    this.searchCategoriesButton.init(this.searchCategories);
-    this.recentSearchKeywords.init(this.searchBar);
-    this.automaticCompletion.init(this.searchBar);
-    this.searchBar.init(this.recentSearchKeywords, this.automaticCompletion);
-  }
-}
+Header.prototype.initSearchArea = function () {
+  this.searchCategories.init(this.searchCategoriesButton);
+  this.searchCategoriesButton.init(this.searchCategories);
+  this.recentSearchKeywords.init(this.searchBar);
+  this.automaticCompletion.init(this.searchBar);
+  this.searchBar.init(this.recentSearchKeywords, this.automaticCompletion);
+};
