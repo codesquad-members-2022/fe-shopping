a61c26f90d56393dc27b71be67b06c6e703404e9
Hemudi <ksum1205@naver.com>
2022-03-23 11:40:06 +0900

    Feat: async await 이용한 fetchData 함수 구현

---
+++ public/src/util/utils.js
@@ -0,0 +1,9 @@
+export const fetchData = async (url) => {
+    const response = await fetch(url);
+    if (response.status === 200) {
+        const data = await response.json();
+        return data;
+    } else {
+        throw new Error('Fetch Failed');
+    }
+}
