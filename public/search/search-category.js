925180d295d5675cda8a4267c615ed9e88b8984b
nanjeong <whdwkxj7@naver.com>
2022-03-24 16:47:23 +0900

    fix: 카테고리 클릭시 카테고리 인덱스 변경하도록 수정

--- public/search/search-category.js
+++ public/search/search-category.js
@@ -15,23 +15,33 @@ export class SearchCategory {
         this.curIdx = -1;
     }
 
+    showArrow(arrow) {
+        arrow.style.display = "block";
+    }
+
+    hideArrow(arrow) {
+        arrow.style.display = "none";
+    }
+
     show() {
         this.isVisible = true;
         this.categoryList.classList.replace("roll-up", "roll-down");
-        this.arrowDown.style.display = "none";
-        this.arrowUp.style.display = "block";
+        this.showArrow(this.arrowUp);
+        this.hideArrow(this.arrowDown);
+        this.focusItem();
     }
 
     hide() {
         this.isVisible = false;
         this.categoryList.classList.replace("roll-down", "roll-up");
-        this.arrowDown.style.display = "block";
-        this.arrowUp.style.display = "none";
+        this.showArrow(this.arrowDown);
+        this.hideArrow(this.arrowUp);
     }
 
     changeCategory(newCategory) {
         this.selectedCategoryName = newCategory.dataset.categoryName;
         this.selectedCategory = newCategory.dataset.category;
+        this.curIdx = Number(newCategory.dataset.idx);
         this.categorySelected.innerText = this.selectedCategoryName;
     }
 
