4e4d7c7e587498cb5099bc19cfdc43faca776439
suuzin <8oozin@gmail.com>
2022-03-25 13:40:07 +0900

    feat : 검색창에 입력된 내용 html에 추가

--- public/js/recentSearch.js
+++ public/js/recentSearch.js
@@ -11,6 +11,17 @@ export class HistorySearch {
     this.storage = localStorage;
   }
 
+  saveSearchHistory(searchInputValue) {
+    const { id, searchContent } = searchInputValue;
+    const addSearchHistory = document.createElement("li");
+    const deleteBtn = document.createElement("span");
+    addSearchHistory.id = id;
+    addSearchHistory.innerText = searchContent;
+    deleteBtn.innerText = "x";
+    addSearchHistory.appendChild(deleteBtn);
+    searchListHistory.appendChild(addSearchHistory);
+  }
+
   inputSubmitHandler(event) {
     event.preventDefault();
     const searchInputValue = searchInput.value;
