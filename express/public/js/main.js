4bc71367d0763de7cd7434502b98d1ea8197c332
seungchanHan <seungchanhan00@gmail.com>
2022-03-25 14:27:49 +0900

    [Add] smart category 연결

--- express/public/js/main.js
+++ express/public/js/main.js
@@ -1,15 +1,15 @@
 import Dropdown from './search/dropdown.js';
 import InputEvent from './search/input-form/input-event.js';
 import Carousel from './carousel/carousel.js';
-import Category from './category/category.js';
+import CategoryPresenter from './category/category-presenter.js';
 
 (() => {
   const dropdown = new Dropdown();
   const inputEvent = new InputEvent();
   const carousel = new Carousel();
-  const category = new Category();
+  const presenter = new CategoryPresenter();
   dropdown.addBtnEvent();
   inputEvent.addInputEvent();
   carousel.startSlide();
-  category.addMouseEvent();
+  presenter.categoryInit();
 })();
