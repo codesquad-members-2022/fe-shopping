7d5963b51f9f807d9166bf3fec24585d2ebf0892
suuzin <8oozin@gmail.com>
2022-03-25 13:54:30 +0900

    feat : 아마존 자동완성 api 가져오기

--- public/js/autocompletekeyword.js
+++ public/js/autocompletekeyword.js
@@ -6,6 +6,14 @@ export class Autocompletekeyword {
     this.getDebounce();
   }
 
+  getAmazonAPI(data) {
+    fetch(
+      `https://completion.amazon.com/api/2017/suggestions?session-id=133-4736477-7395454&customer-id=&request-id=4YM3EXKRH1QJB16MSJGT&page-type=Gateway&lop=en_US&site-variant=desktop&client-info=amazon-search-ui&mid=ATVPDKIKX0DER&alias=aps&b2b=0&fresh=0&ks=71&prefix=${data}&event=onKeyPress&limit=11&fb=1&suggestion-type=KEYWORD`
+    )
+      .then((response) => response.json())
+      .then((data) => this.compareKeyword(data));
+  }
+
   getDebounce() {
     let _this = this;
     searchInput.addEventListener("input", function (event) {
