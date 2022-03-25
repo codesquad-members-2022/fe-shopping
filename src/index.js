d4f11a17a9c64cb0d571dd06c178547464930450
DESKTOP-88VMMCI\user <luzverde0314@gmail.com>
2022-03-25 14:22:00 +0900

    Feat: 실행 로직 수정 및 main함수 생성

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
 import { SearchCategoryController } from "./controller/header/search-category-controller.js";
 import { SearchInputController } from "./controller/header/search-input-controller.js";
 import { HeaderMain } from "./view/header/header-main.js";
-import { SearchInput } from "./view/header/search-input.js";
-import { SearchCategory } from "./view/header/search.-category.js";
 import { TopBar } from "./view/header/topbar.js";
 
-const topbar = new TopBar();
-const headerMain = new HeaderMain();
-const searchCategory = new SearchCategory();
-const searchInput = new SearchInput();
+const main = () => {
+  const topbar = new TopBar();
+  const headerMain = new HeaderMain();
+  const categoryController = new SearchCategoryController();
+  const inputController = new SearchInputController();
 
-topbar.render(topbar.template, ".header-container");
-headerMain.render(headerMain.template, ".header-container");
-searchCategory.render(searchCategory.template, ".search-wrap");
-searchInput.render(searchInput.template, ".search-wrap");
+  topbar.render(topbar.template, ".header-container");
+  headerMain.render(headerMain.template, ".header-container");
+  categoryController.init();
+  inputController.init();
+};
 
-const categoryController = new SearchCategoryController();
-const inputController = new SearchInputController();
-categoryController.addEvents();
-inputController.addEvents();
+main();
