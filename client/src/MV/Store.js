eaa3789241d2a0915a553331283efe7330c6bbdc
godhyeongman <aaaa4000@naver.com>
2022-03-25 11:30:20 +0900

    refactor: index.js 객체 연결 수정

--- client/src/MV/Store.js
+++ client/src/MV/Store.js
@@ -1,18 +1,17 @@
 class Store {
-  constructor(model, view) {
+  constructor({ model, view }) {
     this.model = model;
-    this.veiw = view;
+    this.view = view;
     this.prevState = null; // 사용하고 싶지만 고칠게 많아서 주말로 미룹니다.
     this.nextState = null;
   }
 
-  reduceData(data) {
-    this.model;
-    this.nextState = this.model.getHTML(data);
+  reduceData(data, model) {
+    this.nextState = model.getHTML(data);
   }
 
-  renderNextState() {
-    this.view.render(this.nextState);
+  renderNextState(view) {
+    view.render(this.nextState);
   }
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import { SearchInputEventHandler } from "./EventHandler/header/SearchInputHandle
 import { SearchMenuEventHandler } from "./EventHandler/header/SearchMenuHandler.js";
 import { HeaderRoutes } from "./routes/HeaderRoutes.js";
 import { HeaderHistoryPatcher } from "./service/dataController/header/HistoryManager.js";
-import { HedearDataDispatcher } from "./service/dataController/header/DataDispatcher.js";
 import { HeaderKeyboadManager } from "./service/keyInputController/HeaderKeyManager.js";
 import { SearchInputView } from "./View/header/SearchInputView.js";
 import { SearchMenuView } from "./View/header/searchMenuView.js";
@@ -24,16 +23,32 @@ const SEARCH_INPUT_VIEW = new SearchInputView(".header__main--inputWrapper");
 const SEARCH_INPUT_MODEL = new SearchInputToggle();
 const SEARCH_MENU_VIEW = new SearchMenuView(".header__main--inputMenuButton");
 const SEARCH_MENU_MODEL = new SearchMenuToggle();
-const ETC_MOVEMENT = new ETCmovement(SEARCH_INPUT_VIEW, SEARCH_MENU_VIEW);
-const INPUT_OBSERVER = new ObserverPublisher(null);
-const MENU_OBSERVER = new ObserverPublisher(null);
 
+const ETC_MOVEMENT = new ETCmovement(SEARCH_INPUT_VIEW, SEARCH_MENU_VIEW); // 추후 삭제 예정
 const HEADER_KEY_MANAGER = new HeaderKeyboadManager(
   SEARCH_INPUT_VIEW,
   SEARCH_MENU_VIEW
-); // 추후 삭제 가능
+); // 추후 삭제 예정
+
 const HISTORY_MANAGER = new HeaderHistoryPatcher(INPUT_OBSERVER);
 const ROUTER = new HeaderRoutes(INPUT_OBSERVER, MENU_OBSERVER);
+
+const INPUT_MV_STORE = new Store({
+  model: SEARCH_INPUT_MODEL,
+  view: SEARCH_INPUT_VIEW,
+});
+const MENU_MV_STORE = new Store({
+  model: SEARCH_MENU_MODEL,
+  view: SEARCH_MENU_VIEW,
+});
+
+const INPUT_OBSERVER = new ObserverPublisher(null);
+const MENU_OBSERVER = new ObserverPublisher(null);
+
+INPUT_OBSERVER.addSubscrive(INPUT_MV_STORE.reduceData);
+INPUT_OBSERVER.addSubscrive(INPUT_MV_STORE.renderNextState);
+INPUT_OBSERVER.addSubscrive(MENU_MV_STORE.reduceData);
+INPUT_OBSERVER.addSubscrive(MENU_MV_STORE.renderNextState);
 
 const mainControllerParams = {
   searchInputHandler: new SearchInputEventHandler(
