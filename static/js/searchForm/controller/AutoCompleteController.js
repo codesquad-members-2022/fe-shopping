85d4ea80ea24ea66f7b74b9bc8d71cd9a735062b
Sangbeom Heo <mansaout@gmail.com>
2022-03-26 02:11:25 +0900

    Feat: autoCompleteView를 조작하는 메소드 추가

--- static/js/searchForm/controller/AutoCompleteController.js
+++ static/js/searchForm/controller/AutoCompleteController.js
@@ -2,10 +2,27 @@ export default class AutoCompleteController {
   constructor({ model, view }) {
     this.model = model;
     this.view = view;
+    this.autoCompleteEl = view.autoCompleteEl;
+    this.listEl = view.listEl;
     this.state = this.model.autoComplete;
+    this.show = 'searchForm__autoComplete--show';
+    this.hidden = 'searchForm__autoComplete--hidden';
   }
 
   init() {
-    this.view.init();
+    this.view.clearAutoComplete = this.clearAutoComplete.bind(this);
+    this.view.showAutoComplete = this.showAutoComplete.bind(this);
+    this.view.hideAutoComplete = this.hideAutoComplete.bind(this);
+  }
+  clearAutoComplete() {
+    this.listEl.innerHTML = '';
+  }
+
+  showAutoComplete() {
+    this.autoCompleteEl.classList.replace(this.hidden, this.show);
+  }
+
+  hideAutoComplete() {
+    this.autoCompleteEl.classList.replace(this.show, this.hidden);
   }
 }
