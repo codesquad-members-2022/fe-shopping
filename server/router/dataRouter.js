b0c8c5d459557b9f0fc506fa6911a71f3e3f69a0
Sangbeom Heo <mansaout@gmail.com>
2022-03-26 01:07:49 +0900

    Feat: 서버에서 자동완성 데이터를 respone 해주는 기능 추가
    - 검색 카테고리와 입력키워드에 맞춰 결과를 응답해주게 만듬
    - 아직 카테고리를 적용시키는 코드는 작성하지 못함

--- server/router/dataRouter.js
+++ server/router/dataRouter.js
@@ -3,8 +3,15 @@ const router = express.Router();
 
 const getUser = () => require('../database/user.json');
 const getSearchCategories = () => require('../database/searchCategories.json');
+const getAutoCompleteWords = () => require('../database/autoComplete.json');
 
 router.get('/user', (req, res) => res.json(getUser().user));
 router.get('/searchCategories', (req, res) => res.json(getSearchCategories().categories));
+router.get('/autoComplete', (req, res) => {
+  const category = req.query.category;
+  const inputValue = req.query.keyword;
+  const words = getAutoCompleteWords()[inputValue];
+  res.json(words);
+});
 
 module.exports = router;
