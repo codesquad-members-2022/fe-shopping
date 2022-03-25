a5ff70facbc3d48207f23bebe2cf291a5c83b2d4
DESKTOP-88VMMCI\user <luzverde0314@gmail.com>
2022-03-25 14:14:19 +0900

    Refactor: 쿼리스트링으로 서버에 요청하고, 서버에서 데이터를 가공하여 받도록 수정

--- src/model/autocomplete-store.js
+++ src/model/autocomplete-store.js
@@ -1,16 +1,13 @@
 export class AutoCompleteStore {
   constructor() {
+    this.keywordData = [];
     this.maxListNum = 10;
+    this.apiURL = "http://127.0.0.1:3000/autocomplete";
   }
-  async setKeywordArr(value) {
-    const data = await fetch("./data/fakeDB.json");
-    const json = await data.json();
-    let sorted = await json.products
-      .filter((v) => v.keyword.includes(value))
-      .sort((a, b) => b.views - a.views);
-    if (sorted.length > this.maxListNum) {
-      sorted = sorted.splice(0, this.maxListNum);
-    }
-    this.keywordData = sorted.map((v) => v.keyword);
+  async setKeywordArr(input) {
+    const data = await fetch(`${this.apiURL}?keyword=${input}`);
+    const result = await data.json();
+
+    this.keywordData = result;
   }
 }
