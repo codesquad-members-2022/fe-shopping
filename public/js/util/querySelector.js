5adba6c11c69a6b4ffa2da7437296c4e0f8ad138
suuzin <8oozin@gmail.com>
2022-03-25 12:45:42 +0900

    refactor : 자주 사용하는 querySelector.js에 따로 저장

---
+++ public/js/util/querySelector.js
@@ -0,0 +1,6 @@
+const searchList = document.querySelector(".header__searchlist");
+const searchInputForm = document.querySelector(".header__searchkeyword form");
+const searchInput = document.querySelector(".header__searchkeyword form input");
+const searchListHistory = document.querySelector(".searchlist__history");
+
+export { searchList, searchInputForm, searchInput, searchListHistory };
