9d96f77e591a262f3d8b259554e2f5298cd22cfc
Muffin <jinlog9@gmail.com>
2022-03-26 23:16:41 +0900

    refactor: clickedAnimation 함수 약간 코드수정

--- client/src/controllers/SearchCategoryController.js
+++ client/src/controllers/SearchCategoryController.js
@@ -1,10 +1,12 @@
 import SearchCategoryView from '../views/search/SearchCategoryView.js';
 import { $, timeDelay, toggleClass } from '../utils/util.js';
 import { searchCategoryData } from '../constants/data.js';
+import { CategoryModel } from '../models/CategoryModel.js';
 
 export class SearchCategoryController {
   constructor() {
     this.SearchCategoryView = new SearchCategoryView();
+    this.categoryModel = new CategoryModel();
     this.SearchCategoryView.clickedAnimation = this.clickedAnimation.bind(this);
 
     this.SearchCategoryView.run();
@@ -12,16 +14,19 @@ export class SearchCategoryController {
 
   async clickedAnimation({ target }) {
     const dataId = target.getAttribute('data-category-id');
+    this.categoryModel.save(searchCategoryData);
+    const localCategoryData = this.categoryModel.getCategoryData();
+
     if (dataId) {
       const allBtn = $('.search--allBtn');
-      allBtn.innerText = searchCategoryData[dataId];
+      allBtn.innerText = localCategoryData[dataId];
     }
 
     const selectListUlBox = $('.search-list');
     if (selectListUlBox === null) {
       this.SearchCategoryView.searchCategoryElement.insertAdjacentHTML(
         'beforeend',
-        this.SearchCategoryView.drawCategoryList(searchCategoryData)
+        this.SearchCategoryView.drawCategoryList(localCategoryData)
       );
       toggleClass(this.SearchCategoryView.searchCategoryElement, 'search--off search--on');
       await timeDelay(1);
