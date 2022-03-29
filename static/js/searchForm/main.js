82bd641d82aa0bc10cba146418b86d6f073bfc81
Sangbeom Heo <mansaout@gmail.com>
2022-03-29 17:04:29 +0900

    Refactor: 각 세부 기능별 main.js 를 없애고 상위 main.js으로 합치기
    전체 로직을 한 눈에 보기 위함
    여러 view를 알고있는 SearchController를 만들기 위함

--- static/js/searchForm/main.js
+++ static/js/searchForm/main.js
@@ -1,25 +1,42 @@
-import { getData } from '../utils/getData.js';
+import { getData } from "../utils/getData.js";
 
-import SearchModel from './model/SearchModel.js';
+import SearchModel from "./SearchModel.js";
 
-import initCategory from './category/main.js';
-import initHistory from './history/main.js';
-import initAutoComplete from './autoComplete/main.js';
-import initInput from './input/main.js';
+import InputView from "./input/InputView.js";
+import CategoryView from "./category/CategoryView.js";
+import HistoryView from "./history/HistoryView.js";
+import AutoCompleteView from "./autoComplete/AutoCompleteView.js";
+
+import InputController from "./input/InputController.js";
+import CategoryController from "./category/CategoryController.js";
+import HistoryController from "./history/HistoryController.js";
+import AutoCompleteController from "./autoComplete/AutoCompleteController.js";
 
 export async function initSearchForm() {
-  const user = await getData('http://127.0.0.1:3000/', 'data', 'user');
-  const searchCategories = await getData('http://127.0.0.1:3000/', 'data', 'searchCategories');
+  const user = await getData("http://127.0.0.1:3000/", "data", "user");
+  const searchCategories = await getData("http://127.0.0.1:3000/", "data", "searchCategories");
 
-  const searchModel = new SearchModel({
+  const initialModelState = {
     mode: user.mode,
     history: user.history,
     categories: searchCategories,
     currentCategory: searchCategories[0],
-  });
+  };
 
-  initCategory(searchModel);
-  initHistory(searchModel);
-  initAutoComplete(searchModel);
-  initInput(searchModel);
+  const searchModel = new SearchModel(initialModelState);
+
+  const inputView = new InputView();
+  const categoryView = new CategoryView();
+  const historyView = new HistoryView();
+  const autoCompleteView = new AutoCompleteView();
+
+  const inputController = new InputController({ model: searchModel, view: inputView });
+  const categoryController = new CategoryController({ model: searchModel, view: categoryView });
+  const historyController = new HistoryController({ model: searchModel, view: historyView });
+  const autoCompleteController = new AutoCompleteController({ model: searchModel, view: autoCompleteView });
+
+  inputController.init();
+  categoryController.init();
+  historyController.init();
+  autoCompleteController.init();
 }
