779b3dc22e6583eb7ef65ff5d761ced8fdf7ae83
khj <hongjung.kim.dev@gmail.com>
2022-03-29 20:14:37 +0900

    refactor: controller역할 중에서 model의 상태를view에 맞게 전달하는것을 분리

---
+++ ViewModel/ViewModel.js
@@ -0,0 +1,229 @@
+import {
+  targetQuerySelector,
+  createLiListTemplate,
+  htmlString2htmlElement,
+} from "../util/util.js";
+import { inputData } from "../data/data.js";
+
+class ViewModel {
+  constructor({
+    categoriesDropBox,
+    store,
+    category,
+    searchBarDropBox,
+    searchBar,
+    inputDropBox,
+  }) {
+    this.categoriesDropBox = categoriesDropBox;
+    this.store = store;
+    this.category = category;
+    this.searchBarDropBox = searchBarDropBox;
+    this.searchBar = searchBar;
+    this.inputDropBox = inputDropBox;
+  }
+
+  handleClickSearchCatgory(target) {
+    const $search__category = targetQuerySelector({
+      className: "search__category",
+    });
+
+    if (
+      !this.categoriesDropBox.$search__categories__container.classList.contains(
+        "hidden"
+      )
+    ) {
+      this.categoriesDropBox.render();
+      const $currentCategory = target.closest("li");
+      const selectedCategory = $currentCategory?.textContent;
+      const isInCategoryDropBox =
+        $currentCategory?.parentNode ===
+        this.categoriesDropBox.$search__categories__container;
+
+      if (isInCategoryDropBox) {
+        selectedCategory &&
+          this.store.setState({ ...this.store.state, selectedCategory });
+        this.category.render(this.store.state.selectedCategory);
+      }
+    } else if (
+      target === $search__category ||
+      target === this.category.$selected__category
+    ) {
+      this.categoriesDropBox.render();
+    }
+  }
+
+  appendDropBox(data) {
+    const $categories = createLiListTemplate(data);
+    this.categoriesDropBox.$search__categories__container =
+      htmlString2htmlElement({
+        tag: "ul",
+        htmlString: $categories,
+        className: "search__dropbox",
+      });
+
+    this.category.$selected__category = targetQuerySelector({
+      className: "selected__category",
+    });
+
+    this.category.$selected__category.insertAdjacentElement(
+      "afterend",
+      this.categoriesDropBox.$search__categories__container
+    );
+
+    this.categoriesDropBox.render(this.store.state.isCategoryDropBoxVisible);
+  }
+
+  appendSearchBarDropBox() {
+    this.store.setState({ ...this.store.state, isBarDropBoxVisible: false });
+    const { isBarDropBoxVisible } = this.store.state;
+    this.searchBarDropBox.render({ isBarDropBoxVisible });
+  }
+
+  handleClickOutDropBox() {
+    this.store.setState({ ...this.store.state, isBarDropBoxVisible: false });
+    const { isBarDropBoxVisible } = this.store.state;
+    this.searchBarDropBox.render({ isBarDropBoxVisible });
+  }
+
+  handleKeyupKeywords() {
+    let index = 0;
+    const $ul = this.searchBarDropBox.$search__word__dropbox.children[0];
+    const keywords = $ul.children;
+    const keywordsLen = keywords.length;
+    let previousKeywordIdx = -1;
+
+    this.searchBar.$search.addEventListener("keyup", ({ code }) => {
+      if (code === "ArrowDown") {
+        let currentKeywordIdx = index % keywordsLen;
+        keywords[currentKeywordIdx].style.textDecoration = "underline";
+        this.store.setState({
+          ...this.store.state,
+          keyword: keywords[currentKeywordIdx].textContent,
+        });
+        const { keyword } = this.store.state;
+        this.searchBar.render({ keyword });
+        if (previousKeywordIdx >= 0) {
+          this.store.setState({
+            ...this.store.state,
+            keyupKeyword: [previousKeywordIdx, "none"],
+          });
+          const { keyupKeyword } = this.store.state;
+          this.searchBarDropBox.render({ keyupKeyword });
+        }
+        previousKeywordIdx = currentKeywordIdx;
+        index += 1;
+      } else if (code === "ArrowUp") {
+        let currentKeywordIdx =
+          (index - 1) % keywordsLen === 0 ? index : (index - 1) % keywordsLen;
+        previousKeywordIdx = currentKeywordIdx - 1;
+        if (previousKeywordIdx >= 0 && keywords[previousKeywordIdx]) {
+          this.store.setState({
+            ...this.store.state,
+            keyupKeyword: [previousKeywordIdx, "underline"],
+          });
+          const { keyupKeyword } = this.store.state;
+          this.searchBarDropBox.render({ keyupKeyword });
+        } else if (previousKeywordIdx < 0) {
+          this.store.setState({
+            ...this.store.state,
+            keyupKeyword: [0, "none"],
+          });
+          const { keyupKeyword } = this.store.state;
+          this.searchBarDropBox.render({ keyupKeyword });
+        }
+        if (currentKeywordIdx > 0) {
+          this.store.setState({
+            ...this.store.state,
+            keyupKeyword: [currentKeywordIdx, "none"],
+          });
+          const { keyupKeyword } = this.store.state;
+          this.searchBarDropBox.render({ keyupKeyword });
+        } else {
+          this.store.setState({
+            ...this.store.state,
+            keyupKeyword: [0, "none"],
+          });
+          const { keyupKeyword } = this.store.state;
+          this.searchBarDropBox.render({ keyupKeyword });
+          return;
+        }
+        const keyword = keywords[previousKeywordIdx]
+          ? keywords[previousKeywordIdx].textContent
+          : null;
+
+        this.store.setState({
+          ...this.store.state,
+          keyword,
+        });
+
+        this.searchBar.render({ keyword });
+        currentKeywordIdx = previousKeywordIdx;
+        index -= 1;
+      }
+    });
+  }
+
+  handleFocusInput() {
+    this.store.setState({
+      ...this.store.state,
+      isBarDropBoxVisible: true,
+    });
+    const { isBarDropBoxVisible } = this.store.state;
+    this.searchBarDropBox.render({ isBarDropBoxVisible });
+  }
+
+  handleChangeInput() {
+    this.searchBar.$search.addEventListener("keyup", (event) => {
+      const word = event.target.value;
+      this.searchBarDropBox.$search__word__dropbox.style.visibility = "hidden";
+      let keywordData;
+
+      if (inputData[word]) {
+        keywordData = inputData[word].map(({ keyword }) => keyword);
+      }
+
+      this.inputDropBox?.$search__word__dropbox?.remove(); // Todo: dom추가/제거 방식이 아닌 데이터 바꿀때 리렌더링되도록 개선할 수 있을까?
+
+      if (!keywordData) {
+        const isBarDropBoxVisible = true;
+        this.store.setState({
+          ...this.store.state,
+          isBarDropBoxVisible,
+        });
+        this.searchBarDropBox.render({ isBarDropBoxVisible });
+      }
+
+      const $search__bar = targetQuerySelector({
+        className: "search__bar",
+      });
+
+      let htmlString = "";
+      if (inputData[word] !== undefined) {
+        htmlString = `
+            <ul>
+              ${createLiListTemplate(keywordData)}
+            </ul>
+          `;
+      }
+
+      this.inputDropBox.$search__word__dropbox = htmlString2htmlElement({
+        htmlString,
+        className: "search__word__dropbox",
+      });
+
+      this.inputDropBox.$search__word__dropbox.hasChildNodes() &&
+        $search__bar.insertAdjacentElement(
+          "afterend",
+          this.inputDropBox.$search__word__dropbox
+        );
+      const isBarDropBoxVisible = true;
+      this.store.setState({
+        ...this.store.state,
+        isBarDropBoxVisible,
+      });
+      this.inputDropBox.render(isBarDropBoxVisible);
+    });
+  }
+}
+
+export default ViewModel;
--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 import { searchCategories, searchData } from "./data/data.js";
-import {
-  targetQuerySelector,
-  createLiListTemplate,
-  htmlString2htmlElement,
-} from "./util/util.js";
-import { inputData } from "./data/data.js";
 
 import Store from "./Model/Store.js";
 import SearchCategory from "./View/SearchCategory.js";
@@ -12,6 +6,7 @@ import SearchCategoryDropBox from "./View/SearchCategoryDropBox.js";
 import SearchBar from "./View/SearchBar.js";
 import SearchBarDropBox from "./View/SearchBarDropBox.js";
 import carousel from "./carousel.js";
+import ViewModel from "./ViewModel/ViewModel.js";
 
 const store = new Store();
 
@@ -31,32 +26,47 @@ const {
   searchBarDropBox,
 } = views;
 
+const viewModel = new ViewModel({
+  categoriesDropBox,
+  store,
+  category,
+  searchBarDropBox,
+  searchBar,
+  inputDropBox,
+});
+
 appendDropBoxes({
   appendElement,
   categoriesDropBoxArgs: {
     targetView: categoriesDropBox,
-    source: { data: searchCategories, appendDropBox },
+    source: {
+      data: searchCategories,
+      appendDropBox: viewModel.appendDropBox.bind(viewModel),
+    },
   },
   searchBarDropBoxArgs: {
     targetView: searchBarDropBox,
-    source: { data: searchData, appendSearchBarDropBox },
+    source: {
+      data: searchData,
+      appendSearchBarDropBox: viewModel.appendSearchBarDropBox.bind(viewModel),
+    },
   },
 });
 
 onDropDown({
   onCategoryDropDown: category.onClickSearchCategory,
-  handleClickSearchCatgory,
+  handleClickSearchCatgory: viewModel.handleClickSearchCatgory.bind(viewModel),
   onSearchBarDropDown: searchBarDropBox.onClickDocumentWhenDropDown,
-  handleClickOutDropBox,
+  handleClickOutDropBox: viewModel.handleClickOutDropBox.bind(viewModel),
 });
 
 onSearchBarChange({
   onKeyupKeywords: searchBar.onKeyupKeywords.bind(searchBar),
-  handleKeyupKeywords,
+  handleKeyupKeywords: viewModel.handleKeyupKeywords.bind(viewModel),
   onFocusInput: searchBar.onFocusInput.bind(searchBar),
-  handleFocusInput,
+  handleFocusInput: viewModel.handleFocusInput.bind(viewModel),
   onChangeInput: searchBar.onChangeInput.bind(searchBar),
-  handleChangeInput,
+  handleChangeInput: viewModel.handleChangeInput.bind(viewModel),
 });
 
 function createViews({
@@ -113,208 +123,9 @@ function onSearchBarChange({
   onChangeInput({ handleChangeInput });
 }
 
-function handleClickSearchCatgory(target) {
-  const $search__category = targetQuerySelector({
-    className: "search__category",
-  });
+viewModel.handleClickSearchCatgory();
 
-  if (
-    !categoriesDropBox.$search__categories__container.classList.contains(
-      "hidden"
-    )
-  ) {
-    categoriesDropBox.render();
-    const $currentCategory = target.closest("li");
-    const selectedCategory = $currentCategory?.textContent;
-    const isInCategoryDropBox =
-      $currentCategory?.parentNode ===
-      categoriesDropBox.$search__categories__container;
-
-    if (isInCategoryDropBox) {
-      selectedCategory && store.setState({ ...store.state, selectedCategory });
-      category.render(store.state.selectedCategory);
-    }
-  } else if (
-    target === $search__category ||
-    target === category.$selected__category
-  ) {
-    categoriesDropBox.render();
-  }
-}
-
-function appendDropBox(data) {
-  const $categories = createLiListTemplate(data);
-  categoriesDropBox.$search__categories__container = htmlString2htmlElement({
-    tag: "ul",
-    htmlString: $categories,
-    className: "search__dropbox",
-  });
-
-  category.$selected__category = targetQuerySelector({
-    className: "selected__category",
-  });
-
-  category.$selected__category.insertAdjacentElement(
-    "afterend",
-    categoriesDropBox.$search__categories__container
-  );
-
-  categoriesDropBox.render(store.state.isCategoryDropBoxVisible);
-}
-
-function appendSearchBarDropBox() {
-  store.setState({ ...store.state, isBarDropBoxVisible: false });
-  const { isBarDropBoxVisible } = store.state;
-  searchBarDropBox.render({ isBarDropBoxVisible });
-}
-
-function handleClickOutDropBox() {
-  store.setState({ ...store.state, isBarDropBoxVisible: false });
-  const { isBarDropBoxVisible } = store.state;
-  searchBarDropBox.render({ isBarDropBoxVisible });
-}
-
-function handleKeyupKeywords() {
-  let index = 0;
-  const $ul = searchBarDropBox.$search__word__dropbox.children[0];
-  const keywords = $ul.children;
-  const keywordsLen = keywords.length;
-  let previousKeywordIdx = -1;
-
-  searchBar.$search.addEventListener("keyup", ({ code }) => {
-    if (code === "ArrowDown") {
-      let currentKeywordIdx = index % keywordsLen;
-      keywords[currentKeywordIdx].style.textDecoration = "underline";
-      store.setState({
-        ...store.state,
-        keyword: keywords[currentKeywordIdx].textContent,
-      });
-      const { keyword } = store.state;
-      searchBar.render({ keyword });
-      if (previousKeywordIdx >= 0) {
-        store.setState({
-          ...store.state,
-          keyupKeyword: [previousKeywordIdx, "none"],
-        });
-        const { keyupKeyword } = store.state;
-        searchBarDropBox.render({ keyupKeyword });
-      }
-      previousKeywordIdx = currentKeywordIdx;
-      index += 1;
-    } else if (code === "ArrowUp") {
-      let currentKeywordIdx =
-        (index - 1) % keywordsLen === 0 ? index : (index - 1) % keywordsLen;
-      previousKeywordIdx = currentKeywordIdx - 1;
-      if (previousKeywordIdx >= 0 && keywords[previousKeywordIdx]) {
-        store.setState({
-          ...store.state,
-          keyupKeyword: [previousKeywordIdx, "underline"],
-        });
-        const { keyupKeyword } = store.state;
-        searchBarDropBox.render({ keyupKeyword });
-      } else if (previousKeywordIdx < 0) {
-        store.setState({
-          ...store.state,
-          keyupKeyword: [0, "none"],
-        });
-        const { keyupKeyword } = store.state;
-        searchBarDropBox.render({ keyupKeyword });
-      }
-      if (currentKeywordIdx > 0) {
-        store.setState({
-          ...store.state,
-          keyupKeyword: [currentKeywordIdx, "none"],
-        });
-        const { keyupKeyword } = store.state;
-        searchBarDropBox.render({ keyupKeyword });
-      } else {
-        store.setState({
-          ...store.state,
-          keyupKeyword: [0, "none"],
-        });
-        const { keyupKeyword } = store.state;
-        searchBarDropBox.render({ keyupKeyword });
-        return;
-      }
-      const keyword = keywords[previousKeywordIdx]
-        ? keywords[previousKeywordIdx].textContent
-        : null;
-
-      store.setState({
-        ...store.state,
-        keyword,
-      });
-
-      searchBar.render({ keyword });
-      currentKeywordIdx = previousKeywordIdx;
-      index -= 1;
-    }
-  });
-}
-
-function handleFocusInput() {
-  store.setState({
-    ...store.state,
-    isBarDropBoxVisible: true,
-  });
-  const { isBarDropBoxVisible } = store.state;
-  searchBarDropBox.render({ isBarDropBoxVisible });
-}
-
-function handleChangeInput() {
-  searchBar.$search.addEventListener("keyup", (event) => {
-    const word = event.target.value;
-    searchBarDropBox.$search__word__dropbox.style.visibility = "hidden";
-    let keywordData;
-
-    if (inputData[word]) {
-      keywordData = inputData[word].map(({ keyword }) => keyword);
-    }
-
-    inputDropBox?.$search__word__dropbox?.remove(); // Todo: dom추가/제거 방식이 아닌 데이터 바꿀때 리렌더링되도록 개선할 수 있을까?
-
-    if (!keywordData) {
-      const isBarDropBoxVisible = true;
-      store.setState({
-        ...store.state,
-        isBarDropBoxVisible,
-      });
-      searchBarDropBox.render({ isBarDropBoxVisible });
-    }
-
-    const $search__bar = targetQuerySelector({
-      className: "search__bar",
-    });
-
-    let htmlString = "";
-    if (inputData[word] !== undefined) {
-      htmlString = `
-      <ul>
-        ${createLiListTemplate(keywordData)}
-      </ul>
-    `;
-    }
-
-    inputDropBox.$search__word__dropbox = htmlString2htmlElement({
-      htmlString,
-      className: "search__word__dropbox",
-    });
-
-    inputDropBox.$search__word__dropbox.hasChildNodes() &&
-      $search__bar.insertAdjacentElement(
-        "afterend",
-        inputDropBox.$search__word__dropbox
-      );
-    const isBarDropBoxVisible = true;
-    store.setState({
-      ...store.state,
-      isBarDropBoxVisible,
-    });
-    inputDropBox.render(isBarDropBoxVisible);
-  });
-}
-
-const $slideList = document.querySelector(".slide__list");
-const $banner__category = document.querySelector(".banner__category");
-
-carousel({ slides: $slideList, selector: $banner__category });
+carousel({
+  slides: document.querySelector(".slide__list"),
+  selector: document.querySelector(".banner__category"),
+});
