26d35b1149d4faffb1d6c6a55a9a49668c7b543d
Ubeom Kim <kimub1204@gmail.com>
2022-03-29 10:53:27 +0900

    Remove: bar.js _bar.scss 파일 제거

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-import { bar } from './template/bar.js';
+// import { bar } from './template/bar.js';
 import { header } from './template/header.js';
 import { carousel } from './template/carousel.js';
 import { category } from './template/category.js';
@@ -8,9 +8,6 @@ import { autoSearch } from './template/search-auto.js';
 import MainController from './controller/maincontroller.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const wrap = document.querySelector('.wrap');
-  wrap.insertAdjacentHTML('afterbegin', bar);
-
   const container = document.querySelector('.container');
   container.insertAdjacentHTML('afterbegin', header);
   container.insertAdjacentHTML('afterend', carousel);
