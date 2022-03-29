26113d2b4a41bdaecf3df949d6297b784f74d9ec
soralee <sora2821@gmail.com>
2022-03-29 21:19:28 +0900

    Feat: 검색창 카테고리 드롭다운 기능 구현

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,11 @@
 import {SearchBarController} from './controller/searchBarController.js';
+import {SearchCategoryView} from './view/searchCategoryView.js'
 
 init();
 
 function init() {
+  const searchCategoryView = new SearchCategoryView();
   const searchBarController = new SearchBarController();
+  searchCategoryView.init();
   searchBarController.init();
 }
