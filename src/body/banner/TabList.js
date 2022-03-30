af60dfebef7100a506b20e165531c56b702c74c4
Cola <bcad1591@gmail.com>
2022-03-30 12:11:31 +0900

    Feat: mouseover이벤트핸들러 추가
    tab에 mouseover시, 해당 tab의 인덱스번호를 bannerStore의 state.tabNum에 반영

--- src/body/banner/TabList.js
+++ src/body/banner/TabList.js
@@ -1,10 +1,36 @@
-import { selector } from '../../utils/utils.js';
+import { selector, toggleClass } from '../../utils/utils.js';
+import { bannerStore } from './bannerStore.js';
 
 const BANNER_TABLIST = 'banner-tablist';
 const BANNER_TAB = 'banner-tab';
+const SELECTED = 'is-selected';
 
 export class TabList {
   constructor() {
     this.$bannerTabList = selector(`.${BANNER_TABLIST}`);
+    this.init();
   }
+
+  init() {
+    this.$bannerTabList.addEventListener('mouseover', this.handleMouseOver);
+  }
+
+  update(tabNum) {
+    const $prevTab = selector(`.${SELECTED}`, this.$bannerTabList);
+    const $curTab = this.$bannerTabList.children[tabNum];
+
+    toggleClass(SELECTED, $prevTab);
+    toggleClass(SELECTED, $curTab);
+  }
+
+  handleMouseOver = (e) => {
+    const $bannerTab = e.target.closest(`.${BANNER_TAB}`);
+    if (!$bannerTab) return;
+    const selectedTabNum = Array.prototype.indexOf.call(
+      this.$bannerTabList.children,
+      $bannerTab
+    );
+
+    bannerStore.setTabNum(selectedTabNum);
+  };
 }
