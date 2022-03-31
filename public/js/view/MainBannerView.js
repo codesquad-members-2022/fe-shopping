79dee5cfa167fabfa936a2eedf58fc06a0ae63bb
mina-gwak <mina.gwak@kakao.com>
2022-04-01 03:31:12 +0900

    feat: Change banner automatically

--- public/js/view/MainBannerView.js
+++ public/js/view/MainBannerView.js
@@ -9,26 +9,47 @@ class MainBannerView extends Component {
   }
 
   template() {
-    const selectedBanner = this.viewModel.getSelectedBanner();
-    const banners = this.viewModel.getAllBanners();
+    this.selectedBanner = this.viewModel.getSelectedBanner();
+    this.banners = this.viewModel.getAllBanners();
 
     return `<a href="#" class="banner-img">
-              <img src="${selectedBanner.bannerImg}" alt="${selectedBanner.item}" />
+              <img src="${this.selectedBanner.bannerImg}" alt="${this.selectedBanner.item}" />
             </a>
             <ul class="banner-category">
-              ${banners.reduce((template, { item, categoryImg }) => {
+              ${this.banners.reduce((template, { item, categoryImg }) => {
                 return template += `<li class="category-item">
-                  <a href="#" class="category-link ${item === selectedBanner.item ? 'focus' : ''}">${item}</a>
+                  <a href="#" class="category-link ${item === this.selectedBanner.item ? 'focus' : ''}">${item}</a>
                   <img src="${categoryImg}" alt="${item}" />
-                </li>`
+                </li>`;
               }, '')}
             </ul>`;
   }
 
   setEvent() {
-    this.addEvent('mouseover', '.banner-category', (event) => {
-      this.viewModel.changeSelectedBanner(event.target.innerText);
-    })
+    this.addEvent('mouseover', '.banner-category', ({ target }) => {
+      this.stopBannerChanging();
+      this.viewModel.changeSelectedBanner(target.innerText);
+    });
+  }
+
+  mounted() {
+    this.stopBannerChanging = this.changeBanner();
+  }
+
+  changeBanner() {
+    let flag = true;
+    setTimeout(() => {
+      if (!flag) return;
+      const nextBanner = this.getNextBanner();
+      this.viewModel.changeSelectedBanner(nextBanner);
+    }, 2000);
+    return () => { flag = false };
+  }
+
+  getNextBanner() {
+    let currentIndex = this.banners.findIndex(banner => banner.item === this.selectedBanner.item);
+    const nextIndex = currentIndex === this.banners.length - 1 ? 0 : ++currentIndex;
+    return this.banners[nextIndex].item;
   }
 }
 
